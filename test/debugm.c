dbcaf541b60b27012746c0d680033ed3e3bcf6ce
Nigel Horne <njh@clamav.net>
2003-12-14 12:06:47 +0000

    no longer uses sigint
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@157 77e5149b-7576-45b1-b177-96237e5ba77b

---
+++ test/debugm.c
@@ -0,0 +1,46 @@
+/*
+ * $CC $CFLAGS mboxtest.c -lclamav -lefence (or what ever memory debugger)
+ *
+ * njh@bandsman.co.uk
+ */
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <malloc.h>
+#include <clamav.h>
+#include <sys/resource.h>
+
+int
+main(int argc, char **argv)
+{
+	struct rlimit rlim;
+
+	if(argc == 1) {
+		fprintf(stderr, "Usage: %s files...\n", argv[0]);
+		return 1;
+	}
+	rlim.rlim_cur = rlim.rlim_max = RLIM_INFINITY;
+	if(setrlimit(RLIMIT_CORE, &rlim) < 0)
+		perror("setrlimit");
+
+	if(mkdir("/tmp/mboxtest", 0750) < 0) {
+		perror("/tmp/mboxtest");
+		return errno;
+	}
+	while(*++argv) {
+		int fd = open(*argv, 0);
+
+		if(fd < 0) {
+			perror(*argv);
+			return errno;
+		}
+		printf("cl_mbox(%s) returns %d\n",
+			*argv, cl_mbox("/tmp/mboxtest", fd));
+		close(fd);
+	}
+	puts("Finished - don't forget to rm -rf /tmp/mboxtest");
+
+	exit(0);
+}
