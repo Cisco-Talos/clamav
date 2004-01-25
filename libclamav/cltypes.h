35795bcaa7ca19155514586ae80b874748e8cd0c
Tomasz Kojm <tkojm@clamav.net>
2004-01-25 21:06:56 +0000

    *** empty log message ***
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@207 77e5149b-7576-45b1-b177-96237e5ba77b

---
+++ libclamav/cltypes.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2004 Tomasz Kojm <tkojm@clamav.net>
+ *
+ *  Based on zzip-stdint.h
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef __CLTYPES_H
+#define __CLTYPES_H
+
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#elif defined HAVE_SYS_INT_TYPES_H /*solaris*/
+#include <sys/int_types.h>
+#elif defined HAVE_INTTYPES_H /*freebsd*/
+#include <inttypes.h>
+#else
+    typedef unsigned char uint8_t;      typedef signed char int8_t;
+
+# if SIZEOF_INT == 2
+    typedef unsigned int uint16_t;      typedef signed int int16_t;
+# elif SIZEOF_SHORT == 2
+    typedef unsigned short uint16_t;    typedef signed short int16_t;
+# else
+#   error unable to typedef int16_t from either int or short
+    typedef unsigned short uint16_t;    typedef signed short int16_t;
+# endif
+
+# if defined SIZEOF_INT == 4
+    typedef unsigned int uint32_t;      typedef signed int int32_t;
+# elif defined SIZEOF_LONG == 4
+    typedef unsigned long uint32_t;     typedef signed long int32_t;
+# else
+#   error unable to typedef int32_t from either int or long
+    typedef unsigned long uint32_t;     typedef signed long int32_t;
+# endif
+#endif
+
+#endif
