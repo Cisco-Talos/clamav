c94097feb298ea4c12c926882ea6c38f830a49f7
Tomasz Kojm <tkojm@clamav.net>
2004-01-27 21:39:13 +0000

    Support VirusEvent in clamuko
    git-svn: trunk@215

--- clamav-devel/clamd/clamuko.c
+++ clamav-devel/clamd/clamuko.c
@@ -184,6 +184,7 @@ void *clamukoth(void *arg)
 
 	    if(scan && cl_scanfile(acc.filename, &virname, NULL, tharg->root, tharg->limits, options) == CL_VIRUS) {
 		logg("Clamuko: %s: %s FOUND\n", acc.filename, virname);
+		virusaction(acc.filename, virname, tharg->copt);
 		acc.deny = 1;
 	    } else
 		acc.deny = 0;
