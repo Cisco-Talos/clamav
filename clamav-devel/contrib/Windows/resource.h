f6939ea5de8610b162dd5be443f4963d60bc0ba5
Nigel Horne <njh@clamav.net>
2004-04-08 21:56:41 +0000

    First open source version
    git-svn: trunk@480

---
+++ clamav-devel/contrib/Windows/resource.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (C) 2004 Nigel Horne <njh@bandsman.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by clamav.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_SERVERNAME                  130
+#define IDD_PROGRESS                    131
+#define IDD_OPTIONS                     132
+#define IDC_SERVERNAME                  1000
+#define IDSTOP                          1001
+#define IDC_FileName                    1002
+#define IDC_Percent                     1003
+#define IDC_RECURSIVE                   1004
+#define IDC_QUARANTINE                  1005
+#define IDC_SERVERPORT                  1006
+#define ID_SET_OPTIONS                  32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
