6307ca155cbab7bedfe6f240a4c85214a9d747dd
Tomasz Kojm <tkojm@clamav.net>
2004-05-11 23:30:57 +0000

    add PE dumper
    git-svn: trunk@556

--- clamav-devel/libclamav/pe.h
+++ clamav-devel/libclamav/pe.h
@@ -1,6 +1,5 @@
 /*
- *  Copyright (C) 2002 - 2004 Tomasz Kojm <tkojm@clamav.net>
- *  With enhancements from Thomas Lamy <Thomas.Lamy@in-online.net>
+ *  Copyright (C) 2004 Tomasz Kojm <tkojm@clamav.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,22 +16,11 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#ifndef __SCANNERS_H
-#define __SCANNERS_H
+#ifndef __PE_H
+#define __PE_H
 
-typedef enum {
-    CL_UNKNOWN_TYPE = 0,
-    CL_DOSEXE,
-    CL_DATAFILE,
-    CL_MAILFILE,
-    CL_GZFILE,
-    CL_ZIPFILE,
-    CL_BZFILE,
-    CL_RARFILE,
-    CL_MSCFILE,
-    CL_OLE2FILE
-} cli_file_t;
+#include "clamav.h"
 
-cli_file_t cli_filetype(const char *buf, size_t buflen);
+int cli_scanpe(int desc, const char **virname, long int *scanned, const struct cl_node *root, const struct cl_limits *limits, int options, int *reclev);
 
 #endif
