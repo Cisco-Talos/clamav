e0dab58035be8199aff88fe74719b7bc6a26658c
Tomasz Kojm <tkojm@clamav.net>
2004-09-04 13:18:29 +0000

    retry if mirrors are not fully synchronised
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@824 77e5149b-7576-45b1-b177-96237e5ba77b

--- freshclam/freshclam.c
+++ freshclam/freshclam.c
@@ -364,7 +364,7 @@ int download(const struct cfgstruct *copt, const struct optstruct *opt)
 	    ret = downloadmanager(copt, opt, cpt->strarg);
 	    alarm(0);
 
-	    if(ret == 54) {
+	    if(ret == 54 || ret == 59) {
 		if(try < maxattempts - 1) {
 		    mprintf("Trying again...\n");
 		    logg("Trying again...\n");
