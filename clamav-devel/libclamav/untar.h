a8b7c1dd700a29eef0fc2d3db05a288209d7c955
Nigel Horne <njh@clamav.net>
2004-09-05 15:28:10 +0000

    First draft
    git-svn: trunk@829

---
+++ clamav-devel/libclamav/untar.h
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (C) 2004 Nigel Horne <njh@bandsman.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Change History:
+ * $Log: untar.h,v $
+ * Revision 1.1  2004/09/05 15:28:10  nigelhorne
+ * First draft
+ *
+ */
