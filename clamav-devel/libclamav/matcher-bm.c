9199093cd6bb8a6a6d84457db5240a3e06228dec
Tomasz Kojm <tkojm@clamav.net>
2004-09-27 02:04:08 +0000

    update
    git-svn: trunk@930

--- clamav-devel/libclamav/matcher-bm.c
+++ clamav-devel/libclamav/matcher-bm.c
@@ -169,7 +169,7 @@ int cli_bm_scanbuff(const char *buffer, unsigned int length, const char **virnam
 
 		    if(p->target || p->offset) {
 			    int off = offset + i - BM_MIN_LENGTH + BM_BLOCK_SIZE;
-			if(!cli_validatesig(p->target, ftype, p->offset, off, fd, p->virname)) {
+			if(fd == -1 || !cli_validatesig(p->target, ftype, p->offset, off, fd, p->virname)) {
 			    p = p->next;
 			    continue;
 			}
