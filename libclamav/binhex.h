0cd4903c95183f00bc25f0f2406e238381b42dfd
Nigel Horne <njh@clamav.net>
2004-11-18 18:24:45 +0000

    Added binhex.h
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@1109 77e5149b-7576-45b1-b177-96237e5ba77b

---
+++ libclamav/binhex.h
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) 2004 Nigel Horne <njh@bandsman.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Change History:
+ * $Log: binhex.h,v $
+ * Revision 1.2  2004/11/18 18:24:45  nigelhorne
+ * Added binhex.h
+ *
+ */
+
+int	cli_binhex(const char *dir, int desc);
