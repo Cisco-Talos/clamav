36004784ad7cf9a1969a42a0fcbbd7e262883fb7
Tomasz Kojm <tkojm@clamav.net>
2005-01-17 22:32:19 +0000

    increase default compression ratio limit from 200 to 250
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@1252 77e5149b-7576-45b1-b177-96237e5ba77b

--- clamscan/manager.c
+++ clamscan/manager.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2002 - 2004 Tomasz Kojm <tkojm@clamav.net>
+ *  Copyright (C) 2002 - 2005 Tomasz Kojm <tkojm@clamav.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -159,7 +159,7 @@ int scanmanager(const struct optstruct *opt)
     if(optl(opt, "max-ratio"))
         limits->maxratio = atoi(getargl(opt, "max-ratio"));
     else
-        limits->maxratio = 200;
+        limits->maxratio = 250;
 
     /* set options */
 
