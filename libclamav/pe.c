9a5b2c2f18c8bbfaf94079229a43ad0c0d5780e7
Tomasz Kojm <tkojm@clamav.net>
2005-03-16 20:59:24 +0000

    fix section handling in petite block
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@1397 77e5149b-7576-45b1-b177-96237e5ba77b

--- libclamav/pe.c
+++ libclamav/pe.c
@@ -1177,7 +1177,7 @@ int cli_scanpe(int desc, const char **virname, long int *scanned, const struct c
     }
 
     if(buff[0] != '\xb8' || cli_readint32(buff + 1) != EC32(section_hdr[nsections - 1].VirtualAddress) + EC32(optional_hdr.ImageBase)) {
-	if(buff[0] != '\xb8' || (nsections > 1 && cli_readint32(buff + 1) != EC32(section_hdr[nsections - 2].VirtualAddress) + EC32(optional_hdr.ImageBase)))
+	if(nsections < 2 || buff[0] != '\xb8' || cli_readint32(buff + 1) != EC32(section_hdr[nsections - 2].VirtualAddress) + EC32(optional_hdr.ImageBase))
 	    found = 0;
 	else
 	    found = 1;
