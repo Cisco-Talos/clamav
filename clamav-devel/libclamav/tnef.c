5f1ab8db986217e3c22101e72fcdbe49877b31f7
Nigel Horne <njh@clamav.net>
2005-03-23 08:32:34 +0000

    First draft
    git-svn: trunk@1424

---
+++ clamav-devel/libclamav/tnef.c
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (C) 2005 Nigel Horne <njh@bandsman.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+static	char	const	rcsid[] = "$Id: tnef.c,v 1.1 2005/03/23 08:32:33 nigelhorne Exp $";
+
+#include "tnef.h"
+
+int
+cli_tnef(const char *dir, int desc)
+{
+	return CL_CLEAN;
+}
