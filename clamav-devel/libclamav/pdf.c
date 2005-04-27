d056cc177bf1046d5447529b2898a2e6b99c748b
Nigel Horne <njh@clamav.net>
2005-04-27 12:45:17 +0000

    First draft
    git-svn: trunk@1495

---
+++ clamav-devel/libclamav/pdf.c
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) 2005 Nigel Horne <njh@bandsman.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+static	char	const	rcsid[] = "$Id: pdf.c,v 1.1 2005/04/27 12:45:16 nigelhorne Exp $";
+
+#if HAVE_CONFIG_H
+#include "clamav-config.h"
+#endif
+
+int
+cli_pdf(const char *dir, int desc)
+{
+	return CL_CLEAN;
+}
