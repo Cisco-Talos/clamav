50c12868e0d7363e38ebd0c14e0d5d2e71b6c181
Tomasz Kojm <tkojm@clamav.net>
2005-11-10 18:31:11 +0000

    remove cl_scanbuff
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@1748 77e5149b-7576-45b1-b177-96237e5ba77b

--- libclamav/clamav.h
+++ libclamav/clamav.h
@@ -187,8 +187,6 @@ struct cl_cvd {
 };
 
 /* file scanning */
-extern int cl_scanbuff(const char *buffer, unsigned int length, const char **virname, const struct cl_engine *engine);
-
 extern int cl_scandesc(int desc, const char **virname, unsigned long int *scanned, const struct cl_engine *engine, const struct cl_limits *limits, unsigned int options);
 
 extern int cl_scanfile(const char *filename, const char **virname, unsigned long int *scanned, const struct cl_engine *engine, const struct cl_limits *limits, unsigned int options);
