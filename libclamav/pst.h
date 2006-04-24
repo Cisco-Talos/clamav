fe4eb7235d14886cfaec5ad655610c8e156efa16
Nigel Horne <njh@clamav.net>
2006-04-24 21:19:02 +0000

    First real draft
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@1924 77e5149b-7576-45b1-b177-96237e5ba77b

--- libclamav/pst.h
+++ libclamav/pst.h
@@ -15,6 +15,9 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *  MA 02110-1301, USA.
+ *
+ * Based on libpst version 0.5.1, written by Dave Smith, dave.s at earthcorp.com
+ *	http://alioth.debian.org/projects/libpst/
  */
 
 #ifndef __PST_H
