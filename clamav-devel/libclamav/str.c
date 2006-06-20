2ca4b84e25aaa859981e445721217497acde295e
Tomasz Kojm <tkojm@clamav.net>
2006-06-20 17:43:00 +0000

    update copyrights
    git-svn: trunk@2034

--- clamav-devel/libclamav/str.c
+++ clamav-devel/libclamav/str.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright (C) 2002 - 2005 Tomasz Kojm <tkojm@clamav.net>
+ *  cli_strrcpy(): Copyright (C) 2002 Nigel Horne <njh@bandsman.co.uk>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
