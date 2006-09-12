7dec7955af533ab3c3f0ee812eb04e924f8adff8
aCaB <acab@clamav.net>
2006-09-12 17:50:04 +0000

    Initial merge of the phiscode... more to come.
    git-svn: trunk@2243

---
+++ clamav-devel/libclamav/phish_domaincheck_db.h
@@ -0,0 +1,33 @@
+/*
+ *  Phishing module: domain list implementation.
+ *
+ *  Copyright (C) 2006 Török Edvin <edwintorok@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ *
+ */
+#ifndef _PHISH_DOMAINCHECK_DB_H
+#define _PHISH_DOMAINCHECK_DB_H
+
+int cli_loadpdb(FILE* fd, unsigned int options);
+int build_domainlist(void);
+int init_domainlist(void);
+void domainlist_done(void);
+void domainlist_cleanup(void);
+int is_domainlist_ok(void);
+int domainlist_match(const char* real_url,const char* display_url,int hostOnly,unsigned short* flags);
+
+#endif
