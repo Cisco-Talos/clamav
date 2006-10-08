06d14eefd553a4eeb87c6eeddd415ab443bfe7c7
Tomasz Kojm <tkojm@clamav.net>
2006-10-08 18:03:14 +0000

    fix typo
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@2352 77e5149b-7576-45b1-b177-96237e5ba77b

--- clamscan/treewalk.c
+++ clamscan/treewalk.c
@@ -74,7 +74,7 @@ int treewalk(const char *dirname, struct cl_node *root, const struct passwd *use
 		included = 1;
 		break;
 	    }
-	    argument = opt_nextarg(&optnode, "include");
+	    argument = opt_nextarg(&optnode, "include-dir");
 	}
 
 	if(!included) {
