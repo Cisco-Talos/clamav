525f4b84143ad6e243477f374c756f094cfa7c01
Tomasz Kojm <tkojm@clamav.net>
2006-10-15 00:00:32 +0000

    rename options
    git-svn: trunk@2380

--- clamav-devel/clamscan/clamscan.c
+++ clamav-devel/clamscan/clamscan.c
@@ -254,7 +254,7 @@ void help(void)
     mprintf("    --no-phishing                        Disable phishing detection\n");
 #ifdef CL_EXPERIMENTAL
     mprintf("    --no-phishing-scan-urls              Disable url-based phishing detection\n");
-    mprintf("    --phish-scan-alldomains              Enable phishing detection for all domains (might lead to false positives!)\n");
+    mprintf("    --phishing-strict-url-check          Enable phishing detection for all domains (might lead to false positives!)\n");
 #endif
     mprintf("    --no-algorithmic                     Disable algorithmic detection\n");
     mprintf("    --no-pe                              Disable PE analysis\n");
