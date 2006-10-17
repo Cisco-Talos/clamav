0cf991d247643f4736cdaad6b5902a9bf65f3c3b
Trog <trog@clamav.net>
2006-10-17 13:13:34 +0000

    extract main archive comment for scan
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@2407 77e5149b-7576-45b1-b177-96237e5ba77b

--- libclamav/unrar/unrar.h
+++ libclamav/unrar/unrar.h
@@ -147,6 +147,18 @@ typedef struct file_header_tag
 	off_t next_offset __attribute__ ((packed));
 } file_header_t;
 
+typedef struct comment_header_tag
+{
+	uint16_t head_crc __attribute__ ((packed));
+	uint8_t head_type __attribute__ ((packed));
+	uint16_t flags __attribute__ ((packed));
+	uint16_t head_size __attribute__ ((packed));
+	uint16_t unpack_size __attribute__ ((packed));
+	uint8_t unpack_ver __attribute__ ((packed));
+	uint8_t method __attribute__ ((packed));
+	uint16_t comm_crc __attribute__ ((packed));
+} comment_header_t;
+
 struct Decode
 {
   unsigned int MaxNum;
