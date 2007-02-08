5bb29d460ab2ad2cd70956dc43eec4c582ca8218
Tomasz Kojm <tkojm@clamav.net>
2007-02-08 23:46:37 +0000

    fix db locking under win32 (bb#265)
    git-svn: trunk@2685

--- clamav-devel/libclamav/lockdb.c
+++ clamav-devel/libclamav/lockdb.c
@@ -231,7 +231,7 @@ static int cli_lockdb(const char *dbdirpath, int wait, int writelock)
 	umask(old_mask);
     }
 #else
-    if(!lock->lock_fd) {
+    if(lock->lock_fd != -1) {
 	/* Create a security descriptor which allows any process to acquire the Mutex */
 	InitializeSecurityDescriptor(&sdDesc, SECURITY_DESCRIPTOR_REVISION);
 	SetSecurityDescriptorDacl(&sdDesc, TRUE, NULL, FALSE);
