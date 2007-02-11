38c00cdba33d4dc9190df9363b62db9b2fb8055e
Tomasz Kojm <tkojm@clamav.net>
2007-02-11 10:21:16 +0000

    add option ScriptedUpdates
    git-svn-id: file:///var/lib/svn/clamav-devel/trunk/clamav-devel@2702 77e5149b-7576-45b1-b177-96237e5ba77b

--- shared/cfgparser.c
+++ shared/cfgparser.c
@@ -107,6 +107,7 @@ struct cfgoption cfg_options[] = {
     {"DNSDatabaseInfo", OPT_QUOTESTR, -1, "current.cvd.clamav.net", 0, OPT_FRESHCLAM},
     {"DatabaseMirror", OPT_QUOTESTR, -1, NULL, 1, OPT_FRESHCLAM},
     {"MaxAttempts", OPT_NUM, 3, NULL, 0, OPT_FRESHCLAM},
+    {"ScriptedUpdates", OPT_BOOL, 1, NULL, 0, OPT_FRESHCLAM},
     {"HTTPProxyServer", OPT_QUOTESTR, -1, NULL, 0, OPT_FRESHCLAM},
     {"HTTPProxyPort", OPT_NUM, -1, NULL, 0, OPT_FRESHCLAM},
     {"HTTPProxyUsername", OPT_QUOTESTR, -1, NULL, 0, OPT_FRESHCLAM},
