a4453bce53990975836e150665bc7568fe7feb35
Tomasz Kojm <tkojm@clamav.net>
2007-03-29 15:07:19 +0000

    fix compilation error
    git-svn: trunk@2996

--- clamscan/clamscan_opt.h
+++ clamscan/clamscan_opt.h
@@ -95,7 +95,6 @@ static struct option clamscan_longopt[] = {
 
     /* developers only */
     {"dev-ac-only", 0, 0, 0},
-    {"dev-ac-depth", 1, 0, 0},
 
     {0, 0, 0, 0}
 };
--- clamscan/manager.c
+++ clamscan/manager.c
@@ -187,12 +187,8 @@ int scanmanager(const struct optstruct *opt)
     }
 #endif
 
-    if(opt_check(opt, "dev-ac-only")) {
+    if(opt_check(opt, "dev-ac-only"))
 	dboptions |= CL_DB_ACONLY;
-
-	if(opt_check(opt, "dev-ac-depth"))
-	    cli_ac_setdepth(atoi(opt_arg(opt, "dev-ac-depth")));
-    }
 
     if(opt_check(opt, "database")) {
 	if((ret = cl_load(opt_arg(opt, "database"), &engine, &info.sigs, dboptions))) {
