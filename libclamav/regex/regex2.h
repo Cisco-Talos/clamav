e8dcdb7fac0af3fe0bfbf203864ff571e52f280b
Török Edvin <edwin@clamav.net>
2008-03-10 13:22:16 +0000

    avoid passing out-of-range values to isalnum (thanks Nigel)
    git-svn-id: file:///var/lib/svn/clamav-devel/branches/0.93@3701 77e5149b-7576-45b1-b177-96237e5ba77b

--- libclamav/regex/regex2.h
+++ libclamav/regex/regex2.h
@@ -154,4 +154,4 @@ struct re_guts {
 
 /* misc utilities */
 #define	OUT	(CHAR_MAX+1)	/* a non-character value */
-#define	ISWORD(c)	(isalnum(c) || (c) == '_')
+#define	ISWORD(c)	(isalnum((c)&0xff) || (c) == '_')
