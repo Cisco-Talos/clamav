4cc952e322e1563f7d00fcfb463ed18dba32f0ee
Török Edvin <edwin@clamav.net>
2009-02-12 17:47:36 +0000

    add check for msg.controllen, apparently Mac OS X sets it to 0,     but CMSG_FIRSTHDR is not NULL!
    git-svn: trunk@4761

--- clamd/others.c
+++ clamd/others.c
@@ -282,17 +282,19 @@ static int read_fd_data(struct fd_buf *buf)
 	  logg("!control message truncated");
 	  return -1;
       }
-      for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL;
-	   cmsg = CMSG_NXTHDR(&msg, cmsg)) {
-	  if (cmsg->cmsg_len == CMSG_LEN(sizeof(int)) &&
-	      cmsg->cmsg_level == SOL_SOCKET &&
-	      cmsg->cmsg_type == SCM_RIGHTS) {
-	      if (buf->recvfd != -1) {
-		  logg("^Unclaimed file descriptor received. closing\n");
-		  close(buf->recvfd);
+      if (msg.msg_controllen) {
+	  for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL;
+	       cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+	      if (cmsg->cmsg_len == CMSG_LEN(sizeof(int)) &&
+		  cmsg->cmsg_level == SOL_SOCKET &&
+		  cmsg->cmsg_type == SCM_RIGHTS) {
+		  if (buf->recvfd != -1) {
+		      logg("^Unclaimed file descriptor received. closing\n");
+		      close(buf->recvfd);
+		  }
+		  buf->recvfd = *(int *)CMSG_DATA(cmsg);
+		  logg("*RECVMSG: got FD %d\n", buf->recvfd);
 	      }
-	      buf->recvfd = *(int *)CMSG_DATA(cmsg);
-	      logg("*RECVMSG: got FD %d\n", buf->recvfd);
 	  }
       }
   }
