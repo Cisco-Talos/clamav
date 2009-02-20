1005682a83d94ed133c91eaa36d7aad534f19612
Tomasz Kojm <tkojm@clamav.net>
2009-02-20 14:36:53 +0000

    examples/ex1.c: use new API
    git-svn: trunk@4842

--- examples/ex1.c
+++ examples/ex1.c
@@ -1,10 +1,8 @@
 /*
  *  Compilation: gcc -Wall ex1.c -o ex1 -lclamav
  *
- *  Copyright (C) 2007 - 2008 Sourcefire, Inc.
+ *  Copyright (C) 2007 - 2009 Sourcefire, Inc.
  *  Author: Tomasz Kojm <tkojm@clamav.net>
- *
- *  Copyright (C) 2002 - 2006 Tomasz Kojm <tkojm@clamav.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -45,70 +43,63 @@ int main(int argc, char **argv)
 	unsigned int sigs = 0;
 	long double mb;
 	const char *virname;
-	struct cl_engine *engine = NULL;
-	struct cl_limits limits;
+	struct cl_engine *engine;
 
 
     if(argc != 2) {
 	printf("Usage: %s file\n", argv[0]);
-	exit(2);
+	return 2;
     }
 
     if((fd = open(argv[1], O_RDONLY)) == -1) {
 	printf("Can't open file %s\n", argv[1]);
-	exit(2);
+	return 2;
+    }
+
+    if(!(engine = cl_engine_new())) {
+	printf("Can't initialize antivirus engine\n");
+	return 2;
     }
 
     /* load all available databases from default directory */
-    if((ret = cl_load(cl_retdbdir(), &engine, &sigs, CL_DB_STDOPT))) {
+    if((ret = cl_load(cl_retdbdir(), engine, &sigs, CL_DB_STDOPT))) {
 	printf("cl_load: %s\n", cl_strerror(ret));
 	close(fd);
-	exit(2);
+        cl_engine_free(engine);
+	return 2;
     }
 
-    printf("Loaded %d signatures.\n", sigs);
+    printf("Loaded %u signatures.\n", sigs);
 
     /* build engine */
-    if((ret = cl_build(engine))) {
+    if((ret = cl_engine_compile(engine)) != 0) {
 	printf("Database initialization error: %s\n", cl_strerror(ret));;
-	cl_free(engine);
+        cl_engine_free(engine);
 	close(fd);
-	exit(2);
+	return 2;
     }
 
-    /* set up archive limits */
-    memset(&limits, 0, sizeof(struct cl_limits));
-    limits.maxscansize = 100 * 1048576; /* during the scanning of archives this
-					 * size (100 MB) will never be exceeded
-					 */
-    limits.maxfilesize = 10 * 1048576; /* compressed files will only be
-					* decompressed and scanned up to this
-					* size (10 MB)
-					*/
-    limits.maxfiles = 10000; /* max files */
-    limits.maxreclevel = 16; /* maximum recursion level for archives */
-
     /* scan file descriptor */
-    if((ret = cl_scandesc(fd, &virname, &size, engine, &limits, CL_SCAN_STDOPT)) == CL_VIRUS) {
+    if((ret = cl_scandesc(fd, &virname, &size, engine, CL_SCAN_STDOPT)) == CL_VIRUS) {
 	printf("Virus detected: %s\n", virname);
     } else {
 	if(ret == CL_CLEAN) {
 	    printf("No virus detected.\n");
 	} else {
 	    printf("Error: %s\n", cl_strerror(ret));
-	    cl_free(engine);
+	    cl_engine_free(engine);
 	    close(fd);
-	    exit(2);
+	    return 2;
 	}
     }
     close(fd);
+
+    /* free memory */
+    cl_engine_free(engine);
 
     /* calculate size of scanned data */
     mb = size * (CL_COUNT_PRECISION / 1024) / 1024.0;
     printf("Data scanned: %2.2Lf MB\n", mb);
 
-    /* free memory */
-    cl_free(engine);
-
-    exit(ret == CL_VIRUS ? 1 : 0);
+    return ret == CL_VIRUS ? 1 : 0;
 }
