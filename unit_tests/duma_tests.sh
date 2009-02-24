a7d064d8256bb2372fd098d586ab552f894402e8
Török Edvin <edwin@clamav.net>
2009-02-24 21:27:38 +0000

    fix duma timeouts (thanks Nigel!).
    git-svn: trunk@4859

--- unit_tests/duma_tests.sh
+++ unit_tests/duma_tests.sh
@@ -16,34 +16,27 @@ DUMA_OUTPUT_FILE=duma.log
 DUMA_DISABLE_BANNER=1
 LIBPRELOAD="$LIBDUMA"
 rm -f duma.log
+export CK_DEFAULT_TIMEOUT=40
 export DUMA_FILL DUMA_MALLOC_0_STRATEGY DUMA_OUTPUT_FILE DUMA_DISABLE_BANNER LIBPRELOAD
 echo "--- starting clamd under duma to detect overruns"
-CLAMD_WRAPPER=$abs_srcdir/preload_run.sh $abs_srcdir/check_clamd.sh &
-pid1=$!
+CLAMD_WRAPPER=$abs_srcdir/preload_run.sh $abs_srcdir/check_clamd.sh
+if test $? -ne 0; then
+	echo "*** DUMA has detected errors"
+	cat duma.log
+	rm -f duma.log duma2.log
+	exit 1
+fi
 
 echo "--- starting clamd under duma to detect underruns"
 DUMA_OUTPUT_FILE=duma2.log
 DUMA_PROTECT_BELOW=1
 export DUMA_PROTECT_BELOW
 rm -f duma2.log
-CLAMD_TEST_UNIQ1=3 CLAMD_TEST_UNIQ2=4 CLAMD_WRAPPER=$abs_srcdir/preload_run.sh $abs_srcdir/check_clamd.sh&
-pid2=$!
-
-wait $pid1
-exitcode1=$?
-wait $pid2
-exitcode2=$?
-if test $exitcode1 -ne 0; then
-	echo "*** DUMA has detected errors"
-	cat duma.log
-	rm -f duma.log duma2.log
-	exit 1
-fi
-if test $exitcode2 -ne 0; then
+CLAMD_TEST_UNIQ1=3 CLAMD_TEST_UNIQ2=4 CLAMD_WRAPPER=$abs_srcdir/preload_run.sh $abs_srcdir/check_clamd.sh
+if test $? -ne 0; then
 	echo "*** DUMA has detected errors"
 	cat duma2.log
 	rm -f duma.log duma2.log
 	exit 1
 fi
 exit 0
-
