f18dc6078ff1066e6cd28551e071db2996df78e4
Török Edvin <edwin@clamav.net>
2009-08-25 19:09:04 +0300

    Add missing copyright header.

--- libclamav/bytecode2llvm.cpp
+++ libclamav/bytecode2llvm.cpp
@@ -1,3 +1,25 @@
+/*
+ *  Load, and verify ClamAV bytecode.
+ *
+ *  Copyright (C) 2009 Sourcefire, Inc.
+ *
+ *  Authors: Török Edvin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+
 #include "llvm/Support/DataTypes.h"
 #include "llvm/System/Threading.h"
 #include "llvm/Support/ErrorHandling.h"
