0f505d61f8c1940edad50b457512fa8bdfc6457b
Török Edvin <edwin@clamav.net>
2009-09-07 15:57:41 +0300

    Add missing file.

---
+++ libclamav/bytecode_api_impl.h
@@ -0,0 +1,27 @@
+/*
+ *  ClamAV bytecode internal API
+ *
+ *  Copyright (C) 2009 Sourcefire, Inc.
+ *
+ *  Authors: Török Edvin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+struct cli_bc_ctx;
+uint32_t cli_bcapi_test0(struct cli_bc_ctx *, struct foo*, uint32_t);
+uint32_t cli_bcapi_test1(struct cli_bc_ctx *, uint32_t, uint32_t);
+
+int32_t cli_bcapi_read(struct cli_bc_ctx*, uint8_t *data, int32_t size);
+int32_t cli_bcapi_seek(struct cli_bc_ctx*, int32_t pos, uint32_t whence);
