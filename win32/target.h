4ad03e802679e5ebb4a8866a87d0e5bf60125e4d
aCaB <acab@clamav.net>
2009-10-30 12:01:05 +0100

    add forgotten win32/target.h

---
+++ win32/target.h
@@ -0,0 +1,11 @@
+#ifndef TARGET_OS_TYPE
+#define TARGET_OS_TYPE "win32"
+#endif
+
+#ifndef TARGET_ARCH_TYPE
+#define TARGET_ARCH_TYPE "i386"
+#endif
+
+#ifndef TARGET_CPU_TYPE
+#define TARGET_CPU_TYPE "i386"
+#endif
