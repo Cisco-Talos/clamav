15ae50177a9acf0ce6dacaf2ceac38af15c2764c
Tomasz Kojm <tkojm@clamav.net>
2009-11-16 20:08:28 +0100

    fix some compile warnings

--- libclamav/qsort.c
+++ libclamav/qsort.c
@@ -93,16 +93,16 @@ void cli_qsort(void *a, size_t n, size_t es, int (*cmp)(const void *, const void
 loop:	SWAPINIT(a, es);
 	swap_cnt = 0;
 	if (n < 7) {
-		for (pm = a + es; pm < (char *) a + n * es; pm += es)
+		for (pm = (char *) a + es; pm < (char *) a + n * es; pm += es)
 			for (pl = pm; pl > (char *) a && CMP(pl - es, pl) > 0;
 			     pl -= es)
 				swap(pl, pl - es);
 		return;
 	}
-	pm = a + (n / 2) * es;
+	pm = (char *) a + (n / 2) * es;
 	if (n > 7) {
 		pl = a;
-		pn = a + (n - 1) * es;
+		pn = (char *) a + (n - 1) * es;
 		if (n > 40) {
 			d = (n / 8) * es;
 			pl = med3(pl, pl + d, pl + 2 * d, cmp);
@@ -112,9 +112,9 @@ loop:	SWAPINIT(a, es);
 		pm = med3(pl, pm, pn, cmp);
 	}
 	swap(a, pm);
-	pa = pb = a + es;
+	pa = pb = (char *) a + es;
 
-	pc = pd = a + (n - 1) * es;
+	pc = pd = (char *) a + (n - 1) * es;
 	for (;;) {
 		while (pb <= pc && (r = CMP(pb, a)) <= 0) {
 			if (r == 0) {
@@ -140,14 +140,14 @@ loop:	SWAPINIT(a, es);
 		pc -= es;
 	}
 	if (swap_cnt == 0) {  /* Switch to insertion sort */
-		for (pm = a + es; pm < (char *) a + n * es; pm += es)
+		for (pm = (char *) a + es; pm < (char *) a + n * es; pm += es)
 			for (pl = pm; pl > (char *) a && CMP(pl - es, pl) > 0; 
 			     pl -= es)
 				swap(pl, pl - es);
 		return;
 	}
 
-	pn = a + n * es;
+	pn = (char *) a + n * es;
 	r = MIN(pa - (char *)a, pb - pa);
 	vecswap(a, pb - r, r);
 	r = MIN(pd - pc, pn - pd - es);
