ba238db688042c1a63254c628b2362b7b06f16a7
Tomasz Kojm <tkojm@clamav.net>
2009-11-19 14:10:42 +0100

    sigtool/sigtool.c: handle .ign2 files (bb#1625)

--- sigtool/sigtool.c
+++ sigtool/sigtool.c
@@ -81,6 +81,7 @@ static const struct dblist_s {
     { "COPYING",    0 },
     { "daily.cfg",  0 },
     { "daily.ign",  0 },
+    { "daily.ign2",  0 },
     { "daily.ftm",  0 },
     { "main.info",  0 },    { "daily.info", 0 },    { "safebrowsing.info", 0 },
 
