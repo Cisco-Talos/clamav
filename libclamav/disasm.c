2aa9ffe07b7e3cad5b605ac48c84e5e627a4a79d
Török Edvin <edwin@clamav.net>
2009-12-16 15:18:26 +0200

    Revert accidental commit of these files in ec32c565ba43d4a87a218625a1bc1777fdd7bcc2.

--- libclamav/disasm.c
+++ libclamav/disasm.c
@@ -1545,12 +1545,8 @@ static uint8_t *disasm_x86(uint8_t *command, unsigned int len, struct DISASMED *
 	    GETBYTE(b);
 	    shiftme+=b<<(i*8);
 	  }
-	  if (mod) {
-	      shiftme<<=((8-mod)*8);
-	      s->args[reversed].arg.marg.disp=shiftme>>((8-mod)*8);
-	  } else {
-	      s->args[reversed].arg.marg.disp=0;
-	  }
+	  shiftme<<=((8-mod)*8);
+	  s->args[reversed].arg.marg.disp=shiftme>>((8-mod)*8);
 	} else {
 	  if (mod==0 && rm==6) {
 	    s->args[reversed].arg.marg.r1=REG_INVALID;
--- libclamav/spin.c
+++ libclamav/spin.c
@@ -146,7 +146,7 @@ static uint32_t summit (char *src, int size)
       eax ^= ebx>>8 & 0xff;
       eax += 0x7801a108;
       eax ^= ebx;
-      CLI_ROR(eax, ebx&0x1f);
+      CLI_ROR(eax, ebx&0xff);
       swap = eax;
       eax = ebx;
       ebx = swap;
