6e11bf6197fb6ddb25f35527bac427e302c28b42
aCaB <acab@clamav.net>
2010-01-05 19:07:23 +0100

    add copyright stuff

--- libclamav/cache.c
+++ libclamav/cache.c
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2010 Sourcefire, Inc.
+ *
+ *  Authors: aCaB <acab@clamav.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -116,7 +136,6 @@ static int cache_lookup_hash(unsigned char *md5, cli_ctx *ctx) {
 		    e[i].hits++;
 		    cache_swap(e, i);
 		    ret = CL_CLEAN;
-		    cli_warnmsg("cached\n");
 		    break;
 		}
 	    }
