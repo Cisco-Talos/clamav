003b5feec475e76581c2bf8a7081b79c68dff5c2
aCaB <acab@clamav.net>
2010-01-19 14:24:17 +0100

    remove debug printf

--- clamscan/manager.c
+++ clamscan/manager.c
@@ -117,7 +117,6 @@ static int scanfile(const char *filename, struct cl_engine *engine, const struct
 	return 0;
     }
     info.rblocks += fsize / CL_COUNT_PRECISION;
-    printf("THIS: %u(%u) - SO FAR: %u\n", fsize/CL_COUNT_PRECISION, fsize, info.rblocks);
 #ifndef _WIN32
     if(geteuid())
 	if(checkaccess(filename, NULL, R_OK) != 1) {
