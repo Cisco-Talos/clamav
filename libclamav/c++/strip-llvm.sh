5f42f86347689c72e6018376dd2a2e7570256953
Török Edvin <edwin@clamav.net>
2010-03-06 20:41:32 +0200

    Update autogenerated files.

--- libclamav/c++/strip-llvm.sh
+++ libclamav/c++/strip-llvm.sh
@@ -8,6 +8,7 @@ for i in llvm/bindings/ llvm/examples/ llvm/projects/ llvm/runtime/\
     llvm/lib/Target/CellSPU/ llvm/lib/Target/CppBackend/ llvm/lib/Target/Mips\
     llvm/lib/Target/MSIL llvm/lib/Target/MSP430/ llvm/lib/Target/PIC16\
     llvm/lib/Target/Sparc/ llvm/lib/Target/SystemZ llvm/lib/Target/XCore\
+    llvm/lib/Target/MBlaze/ llvm/lib/Target/PIC16/ llvm/lib/Target/MSP430\
     llvm/test/Archive/ llvm/test/Bindings/ llvm/test/Bitcode/ llvm/test/DebugInfo/\
     llvm/test/FrontendAda/ llvm/test/FrontendC llvm/test/FrontendC++/\
     llvm/test/FrontendFortran/ llvm/test/FrontendObjC\
@@ -23,12 +24,13 @@ for i in llvm/bindings/ llvm/examples/ llvm/projects/ llvm/runtime/\
     llvm/tools/llvm-extract llvm/tools/llvm-ld llvm/tools/llvm-link llvm/tools/llvm-mc\
     llvm/tools/llvm-nm llvm/tools/llvm-prof llvm/tools/llvm-ranlib\
     llvm/tools/llvm-stub llvm/tools/lto llvm/tools/opt llvm/lib/MC/MCParser\
-    llvm/tools/llvm-dis/Makefile
+    llvm/tools/llvm-dis/Makefile llvm/tools/edis/ llvm/tools/llvm-shlib\
+    llvm/docs
     do
 	git rm -rf $i;
 done
-# LLVM's config.status depends on these files existing
-mkdir -p llvm/lib/Target/PIC16/AsmPrinter llvm/lib/Target/MSP430/AsmPrinter
-touch llvm/lib/Target/PIC16/AsmPrinter/Makefile llvm/lib/Target/MSP430/AsmPrinter/Makefile
-git add -f llvm/lib/Target/PIC16/AsmPrinter/Makefile llvm/lib/Target/MSP430/AsmPrinter/Makefile
+# config.status needs these
+mkdir -p llvm/docs/doxygen
+touch llvm/docs/doxygen.cfg.in
+git add llvm/docs/doxygen.cfg.in
 mv ../../.git/SQUASH_MSG ../../.git/MERGE_MSG
