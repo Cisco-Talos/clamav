5d2a1c488f8b44b39d5111957114d4d623af1b3b
Török Edvin <edwin@clamav.net>
2010-03-22 11:42:27 +0200

    Update autogenerated files.

--- libclamav/c++/merge.sh
+++ libclamav/c++/merge.sh
@@ -7,7 +7,7 @@ echo "Creating grafts for llvm-upstream"
 
 REPONAME=llvm
 REFPFX=refs/tags/merge-$REPONAME-
-UPSTREAM=$REPONAME-upstream/master
+UPSTREAM=$REPONAME-upstream/release
 git for-each-ref $REFPFX*  --format='%(refname)' | while read tag_ref
 do
 	tag_svn_ref=`echo $tag_ref|sed -e s\|$REFPFX\|\|`
@@ -19,8 +19,15 @@ do
 	echo "$local_ref $local_parent_ref $upstream_ref" >>.git/info/grafts
 done
 echo "Merging llvm-upstream"
-MERGEREV=`git log $UPSTREAM -1 |grep /trunk@|sed -s 's/.*@\([0-9]*\).*/\1/'`
-git merge -s subtree --squash llvm-upstream/master && git commit || {
-echo "Merge failed: resolve conflicts and run: git tag merge-llvm-$MERGEREV && rm .git/info/grafts"; exit 1;}
-git tag merge-llvm-$MERGEREV
-rm .git/info/grafts
+MERGEREV=`git log $UPSTREAM -1 |grep /release_27@|sed -s 's/.*@\([0-9]*\).*/\1/'`
+echo "$MERGEREV"
+git merge -s subtree --squash llvm-upstream/release
+
+echo "Run strip-llvm.sh from libclamav/c++"
+echo "Then fix conflicts if needed: git mergetool"
+echo "Then commit the result and tag it: git commit && git tag merge-llvm-$MERGEREV"
+echo "Then remove the grafts: rm .git/info/grafts"
+# && git commit || {
+# echo "Merge failed: resolve conflicts and run: git tag merge-llvm-$MERGEREV && rm .git/info/grafts"; exit 1;}
+# git tag merge-llvm-$MERGEREV
+# rm .git/info/grafts
