3504bd0e29ba9b900be295924bab98a4264b3d0c
Török Edvin <edwin@clamav.net>
2010-03-24 16:25:57 +0200

    Run check_clamav under valgrind too to actually detect the leaks.
    One leak remaining for IntConstants.

--- unit_tests/check_unit_vg.sh
+++ unit_tests/check_unit_vg.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/sh -x
 . $srcdir/check_common.sh
 init_valgrind
-CK_FORK=no WRAPPER="$VALGRIND $VALGRIND_FLAGS" ./check_clamav
+CK_FORK=no WRAPPER="$VALGRIND $VALGRIND_FLAGS" test_run 0 $TOP/unit_tests/check_clamav
 end_valgrind
