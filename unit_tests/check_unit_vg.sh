95fc03dac72e9149e11bff7b6250d866fbf04e0b
Török Edvin <edwin@clamav.net>
2010-03-24 16:41:29 +0200

    Fix unit test script.
    test_run always exits with 1 on success, so check its return code.

--- unit_tests/check_unit_vg.sh
+++ unit_tests/check_unit_vg.sh
@@ -1,5 +1,8 @@
-#!/bin/sh -x
+#!/bin/sh
 . $srcdir/check_common.sh
 init_valgrind
-CK_FORK=no WRAPPER="$VALGRIND $VALGRIND_FLAGS" test_run 0 $TOP/unit_tests/check_clamav
+export CK_FORK=no WRAPPER="$VALGRIND $VALGRIND_FLAGS" CK_DEFAULT_TIMEOUT=40
+if test_run 0 $TOP/unit_tests/check_clamav; then
+    echo "check_clamav failed to run" >&2
+fi
 end_valgrind
