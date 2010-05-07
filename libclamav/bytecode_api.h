4ed8c2349b5fdafc1a208eb11544dd7291f66178
Török Edvin <edwin@clamav.net>
2010-05-07 11:01:51 +0300

    Restore the fix in clamav-0.96-19-ge4a0f2c.
    It got lost when running sync_clamav in the bytecode compiler.
    Fixed the script now.

--- libclamav/bytecode_api.h
+++ libclamav/bytecode_api.h
@@ -52,7 +52,7 @@ enum BytecodeKind {
     _BC_LAST_HOOK
 };
 
-enum { PE_INVALID_RVA = 0xFFFFFFFF };
+static const unsignedPE_INVALID_RVA = 0xFFFFFFFF };
 
 /** LibClamAV functionality level constants */
 enum FunctionalityLevels {
