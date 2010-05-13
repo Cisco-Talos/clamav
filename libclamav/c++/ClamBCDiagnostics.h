bedba869595ff228d9f099ba94c52ffd59db418b
Török Edvin <edwin@clamav.net>
2010-05-13 12:49:54 +0300

    Add missing file.

---
+++ libclamav/c++/ClamBCDiagnostics.h
@@ -0,0 +1,27 @@
+/*
+ *  Compile LLVM bytecode to ClamAV bytecode.
+ *
+ *  Copyright (C) 2010 Sourcefire, Inc.
+ *
+ *  Authors: Török Edvin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+namespace llvm {
+    class Value;
+}
+// match the prototype used in the bytecode compiler
+void printValue(llvm::Value *V, bool a=false, bool b=false);
+void printLocation(llvm::Instruction *I, bool a=false, bool b=false);
