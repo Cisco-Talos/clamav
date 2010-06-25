e56fca4bbc8e0135176feb9841f29a2d2d34d20e
aCaB <acab@clamav.net>
2010-06-25 22:00:34 +0200

    PLEASE RUN win32/update-win32.pl --regen WHEN YOU TOUCH THE BUILD!!!

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -502,7 +502,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.96-165-gffa9b06"
+#define VERSION "devel-clamav-0.96-177-g44712fc"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
