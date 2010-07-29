669623d5a6f12d93cae4df63fc6c76c530f583bf
Török Edvin <edwin@clamav.net>
2010-07-29 13:43:30 +0300

    Fix computation of type sizes in interpreter.
    It was not computing the size of structs.
    Bug: all bytecodes that had structs on the stack failed with internal
    out-of-bounds errors.
    Workaround: compiler will need to avoid putting structs on stack, or do so only
    for FLEVEL == 0.96.2

--- libclamav/bytecode.c
+++ libclamav/bytecode.c
@@ -222,6 +222,9 @@ int cli_bytecode_context_clear(struct cli_bc_ctx *ctx)
 
 static unsigned typesize(const struct cli_bc *bc, uint16_t type)
 {
+    struct cli_bc_type *ty;
+    unsigned j;
+
     type &= 0x7fff;
     if (!type)
 	return 0;
@@ -233,7 +236,25 @@ static unsigned typesize(const struct cli_bc *bc, uint16_t type)
 	return 4;
     if (type <= 64)
 	return 8;
-    return bc->types[type-65].size;
+    ty = &bc->types[type-65];
+    if (ty->size)
+	return ty->size;
+    switch (ty->kind) {
+	case 2:
+	case 3:
+	    for (j=0;j<ty->numElements;j++)
+		ty->size += typesize(bc, ty->containedTypes[j]);
+	    break;
+	case 4:
+	    ty->size = ty->numElements * typesize(bc, ty->containedTypes[0]);
+	    break;
+	default:
+	    break;
+    }
+    if (!ty->size && ty->kind != DFunctionType) {
+	cli_warnmsg("type %d size is 0\n", type-65);
+    }
+    return ty->size;
 }
 
 static unsigned typealign(const struct cli_bc *bc, uint16_t type)
