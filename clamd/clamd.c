9011cf1ef55eaf8a09b840ebddd28c31adb7a492
Török Edvin <edwin@clamav.net>
2010-08-31 15:10:56 +0300

    Limit RLIMIT_DATA to 2GB on 32-bit processes (bb #1941).
    With RLIMIT_DATA > 2GB a 32-bit FreeBSD 7.3 allows ~120M memory to be allocated
    via mmap before failing.
    With RLIMIT_DATA set to 2GB it allows ~890M memory.
    So check the RLIMIT_DATA and set it just below 2G.
    
    You can also just do 'ulimit -d 2097151' to get the effects of this patch.

--- clamd/clamd.c
+++ clamd/clamd.c
@@ -30,6 +30,7 @@
 #endif
 #ifndef _WIN32
 #include <sys/time.h>
+#include <sys/resource.h>
 #endif
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -108,6 +109,7 @@ int main(int argc, char **argv)
 #ifndef	_WIN32
         struct passwd *user = NULL;
 	struct sigaction sa;
+	struct rlimit rlim;
 #endif
 	time_t currtime;
 	const char *dbdir, *cfgfile;
@@ -294,6 +296,26 @@ int main(int argc, char **argv)
     if(user)
 	logg("#Running as user %s (UID %u, GID %u)\n", user->pw_name, user->pw_uid, user->pw_gid);
 #endif
+
+#ifdef RLIMIT_DATA
+    if (getrlimit(RLIMIT_DATA, &rlim) == 0) {
+       /* bb #1941.
+        * On 32-bit FreeBSD if you set ulimit -d to >2GB then mmap() will fail
+        * too soon (after ~120 MB).
+        * Set limit lower than 2G if on 32-bit */
+       uint64_t lim = rlim.rlim_cur;
+       lim = (int32_t) lim;
+       if (sizeof(void*) == 4 &&
+           lim != rlim.rlim_cur) {
+           rlim.rlim_cur = 2048*1024-1;
+           if (setrlimit(RLIMIT_DATA, &rlim) < 0)
+               logg("!setrlimit(RLIMIT_DATA) failed: %s\n", strerror(errno));
+           else
+               logg("^Running on 32-bit system, and RLIMIT_DATA > 2GB, lowering to 2GB!\n");
+       }
+    }
+#endif
+
 
     if(logg_size)
 	logg("#Log file size limited to %d bytes.\n", logg_size);
