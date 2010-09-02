71ca3536c4e0ed6454315a7c3ad5950ba212fbe1
Török Edvin <edwin@clamav.net>
2010-09-02 21:22:31 +0300

    Fix hung clamd on FreeBSD (bb #2235).
    bytecode selfcheck running under JIT ran too early, and spawned a thread.
    Then clamd forked. threads + fork = bad idea.
    So prevent the thread from being spawned in selfcheck mode.
    So at the time of fork clamd will still be single threaded as in 0.96.1.

--- libclamav/bytecode.c
+++ libclamav/bytecode.c
@@ -2107,6 +2107,7 @@ static int run_selfcheck(struct cli_all_bc *bcs)
     cli_bytecode_context_setfuncid(ctx, bc, 0);
 
     cli_dbgmsg("bytecode self test running\n");
+    ctx->bytecode_timeout = 0;
     rc = cli_bytecode_run(bcs, bc, ctx);
     cli_bytecode_context_destroy(ctx);
     if (rc != CL_SUCCESS) {
--- libclamav/c++/bytecode2llvm.cpp
+++ libclamav/c++/bytecode2llvm.cpp
@@ -1637,11 +1637,15 @@ int cli_vm_execute_jit(const struct cli_all_bc *bcs, struct cli_bc_ctx *ctx,
 	0
     };
 
-    if ((ret = pthread_create(&thread, NULL, bytecode_watchdog, &w))) {
-	errs() << "Bytecode: failed to create new thread!";
-	errs() << cli_strerror(ret, buf, sizeof(buf));
-	errs() << "\n";
-	return CL_EBYTECODE;
+    if (ctx->bytecode_timeout) {
+	/* only spawn if timeout is set.
+	 * we don't set timeout for selfcheck (see bb #2235) */
+	if ((ret = pthread_create(&thread, NULL, bytecode_watchdog, &w))) {
+	    errs() << "Bytecode: failed to create new thread!";
+	    errs() << cli_strerror(ret, buf, sizeof(buf));
+	    errs() << "\n";
+	    return CL_EBYTECODE;
+	}
     }
 
     ret = bytecode_execute((intptr_t)code, ctx);
@@ -1649,7 +1653,9 @@ int cli_vm_execute_jit(const struct cli_all_bc *bcs, struct cli_bc_ctx *ctx,
     w.finished = 1;
     pthread_cond_signal(&w.cond);
     pthread_mutex_unlock(&w.mutex);
-    pthread_join(thread, NULL);
+    if (ctx->bytecode_timeout) {
+	pthread_join(thread, NULL);
+    }
 
     if (cli_debug_flag) {
 	gettimeofday(&tv1, NULL);
