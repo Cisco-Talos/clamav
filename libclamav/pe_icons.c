1f3db7f074995bd4e1d0183b2db8b1c472d2f41b
aCaB <acab@clamav.net>
2010-11-30 12:13:01 +0100

    bb#2344

--- libclamav/pe_icons.c
+++ libclamav/pe_icons.c
@@ -67,7 +67,7 @@ static int icon_cb(void *ptr, uint32_t type, uint32_t name, uint32_t lang, uint3
     struct ICONS *icons = ptr;
     type = type; lang = lang;
     cli_dbgmsg("icon_cb: got icon %x\n", name);
-    if(icons->cnt > 100) 
+    if(icons->cnt >= 100)
 	return 1;
     icons->rvas[icons->cnt] = rva;
     icons->cnt++;
