e3be1cb7d5451beb8447dea5779cd36e3b0080f2
Török Edvin <edwin@clamav.net>
2011-04-14 13:21:27 +0300

    update win32 script

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -505,7 +505,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.97-71-gb9734ed"
+#define VERSION "devel-clamav-0.97-72-gaeee1aa"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
