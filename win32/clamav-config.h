ca2ed64d817a3b065689734c9012a8e9b7da9412
Török Edvin <edwin@clamav.net>
2011-04-14 15:08:25 +0300

    build system: upgrade autoconf

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -427,6 +427,9 @@
 /* The archive extension */
 #define LT_LIBEXT "dll"
 
+/* The archive prefix */
+/* #undef LT_LIBPREFIX */
+
 /* Define to the extension used for runtime loadable modules, say, ".so". */
 #define LT_MODULE_EXT ".dll"
 
@@ -505,7 +508,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.97-77-g14c7f2b"
+#define VERSION "devel-clamav-0.97-79-g404d30a"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
