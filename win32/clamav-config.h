13bf9f6fd2ad1e41d08c8454020146e4491957c4
Török Edvin <edwin@clamav.net>
2011-04-15 16:35:43 +0300

    test commit

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -508,7 +508,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.97-99-g601d635"
+#define VERSION "devel-clamav-0.97-100-g40a60b4"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
