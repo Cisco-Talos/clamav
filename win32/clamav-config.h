d1e76ba1ac2036e9a7621fe43ad639d99e2e8192
Török Edvin <edwin@clamav.net>
2011-04-16 17:41:31 +0300

    Revert "build system: fix clamav-milter makefile"
    This reverts commit 20b7235a9e932150f60c77f7f5703bbd6f0d203e.
    
    Conflicts:
    
    	win32/clamav-config.h

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -508,7 +508,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.97-105-ge1d23d8"
+#define VERSION "devel-clamav-0.97-79-g404d30a"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
