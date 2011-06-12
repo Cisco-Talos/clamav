b21d2db5c5bf7bd39620b6a148eaff20740aa262
Török Edvin <edwin@clamav.net>
2011-06-12 10:07:16 +0300

    logg_size is unsigned

--- clamd/clamd.c
+++ clamd/clamd.c
@@ -318,7 +318,7 @@ int main(int argc, char **argv)
 
 
     if(logg_size)
-	logg("#Log file size limited to %d bytes.\n", logg_size);
+	logg("#Log file size limited to %u bytes.\n", logg_size);
     else
 	logg("#Log file size limit disabled.\n");
 
