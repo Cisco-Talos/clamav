5961eaef591de58a63923826fd347fd385dc4da2
Tom Judge <tjudge@sourcefire.com>
2012-06-13 08:34:12 -0400

    Fix ordering - should fix windows build.

--- libclamav/filetypes.c
+++ libclamav/filetypes.c
@@ -212,8 +212,9 @@ cli_file_t cli_filetype2(fmap_t *map, const struct cl_engine *engine)
 		     * we just tried that above, and failed */
 		    if((encoding = encoding_detect_bom(buff, bread))) {
 			    unsigned char decodedbuff[(MAGIC_BUFFER_SIZE+1)*2];
-             memset(decodedbuff, 0, sizeof(decodedbuff));
 			    m_area_t in_area, out_area;
+			    
+			    memset(decodedbuff, 0, sizeof(decodedbuff));
 
 			    in_area.buffer = (unsigned char *) buff;
 			    in_area.length = bread;
