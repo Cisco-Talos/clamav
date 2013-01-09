9100c3a1ec7e306c62109d631c25fc2dd9f4039b
David Raynor <draynor@sourcefire.com>
2013-01-09 12:58:53 -0500

    bb #6534: SWF signature category

--- libclamav/matcher.h
+++ libclamav/matcher.h
@@ -137,7 +137,7 @@ struct cli_mtarget {
     uint8_t enable_prefiltering;
 };
 
-#define CLI_MTARGETS 11
+#define CLI_MTARGETS 12
 static const struct cli_mtarget cli_mtargets[CLI_MTARGETS] =  {
     { 0,                    "GENERIC",      0,  0, 1 },
     { CL_TYPE_MSEXE,        "PE",           1,  0, 1 },
@@ -149,7 +149,8 @@ static const struct cli_mtarget cli_mtargets[CLI_MTARGETS] =  {
     { CL_TYPE_TEXT_ASCII,   "ASCII",        7,  1, 1 },
     { CL_TYPE_ERROR,        "NOT USED",     8,  1, 0 },
     { CL_TYPE_MACHO,        "MACH-O",       9,  1, 0 },
-    { CL_TYPE_PDF,          "PDF",         10,  1, 0 }
+    { CL_TYPE_PDF,          "PDF",         10,  1, 0 },
+    { CL_TYPE_SWF,          "FLASH",       11,  1, 0 }
 };
 
 #define CLI_OFF_ANY         0xffffffff
