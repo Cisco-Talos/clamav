9b316ff4e46c583363b0869e3bb98eee952862ec
cwu <cwu@sourcefire.com>
2013-02-14 07:28:19 -0800

    fix CID# 10536

--- sigtool/sigtool.c
+++ sigtool/sigtool.c
@@ -2212,6 +2212,7 @@ static char *decodehexspecial(const char *hex, unsigned int *dlen)
 		    }
 		    if(i + 1 != altnum)
 			buff[len++] = '|';
+		    free(c);	
 		}
 		buff[len++] = '}';
 	    }
