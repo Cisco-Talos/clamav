ff611b731455cf3b3d7a172d6a7caceb0032a078
cwu <cwu@sourcefire.com>
2013-02-14 07:39:00 -0800

    fix CID# 10535

--- sigtool/sigtool.c
+++ sigtool/sigtool.c
@@ -2227,6 +2227,7 @@ static char *decodehexspecial(const char *hex, unsigned int *dlen)
 	    }
 	    memcpy(&buff[len], decoded, hlen);
 	    len += hlen;
+	    free(decoded);
 	}
     }
     free(hexcpy);
