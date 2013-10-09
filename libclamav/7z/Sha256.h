2f61f7631380f43c4ad44c3af85f3ff1b38df3ac
Steve Morgan <smorgan@sourcefire.com>
2013-10-09 18:14:33 -0400

    Replace 7zip sha256 with clam sha256

--- libclamav/7z/Sha256.h
+++ libclamav/7z/Sha256.h
@@ -10,6 +10,9 @@ EXTERN_C_BEGIN
 
 #define SHA256_DIGEST_SIZE 32
 
+#define __USE_CLAM_SHA256_FOR_7Z__
+
+#ifndef __USE_CLAM_SHA256_FOR_7Z__
 typedef struct
 {
   UInt32 state[8];
@@ -20,7 +23,13 @@ typedef struct
 void Sha256_Init(CSha256 *p);
 void Sha256_Update(CSha256 *p, const Byte *data, size_t size);
 void Sha256_Final(CSha256 *p, Byte *digest);
-
+#else
+#include "sha256.h"
+#define CSha256 SHA256_CTX
+#define Sha256_Init sha256_init
+#define Sha256_Update sha256_update
+#define Sha256_Final sha256_final
+#endif
 EXTERN_C_END
 
 #endif
