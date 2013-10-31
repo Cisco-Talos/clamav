f2412a222a7d7bfb8bd9757b3e52bbdfadbfe2da
Shawn Webb <swebb@sourcefire.com>
2013-10-31 13:11:26 -0500

    OpenBSD fix #2

--- libclamav/stats.h
+++ libclamav/stats.h
@@ -1,7 +1,7 @@
 #if !defined(_LIBCLAMAV_STATS_H)
 #define _LIBCLAMAV_STATS_H
 
-void clamav_stats_add_sample(const char *virname, const unsigned char *md5, uint64_t size, cli_intel_sample_type_t type, void *cbdata);
+void clamav_stats_add_sample(const char *virname, const unsigned char *md5, size_t size, cli_intel_sample_type_t type, void *cbdata);
 void clamav_stats_submit(struct cl_engine *engine, void *cbdata);
 void clamav_stats_flush(struct cl_engine *engine, void *cbdata);
 void clamav_stats_remove_sample(const char *virname, const unsigned char *md5, size_t size, cli_intel_sample_type_t type, void *cbdata);
