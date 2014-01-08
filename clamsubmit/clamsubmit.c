c60aa96253407d3076e79c8d8dd2ce18e2ece6d9
Shawn Webb <swebb@sourcefire.com>
2014-01-08 11:56:47 -0500

    Switch to using the existing file upload functionality in clamav.net

--- clamsubmit/clamsubmit.c
+++ clamsubmit/clamsubmit.c
@@ -8,16 +8,16 @@
 #include "libclamav/clamav.h"
 #include "libclamav/others.h"
 
-#define OPTS "p:n:i:H:h?"
+#define OPTS "e:p:n:N:H:h?"
 
 void usage(char *name)
 {
     fprintf(stderr, "USAGE: %s -hHinp?\n", name);
     fprintf(stderr, "OPTIONS:\n");
+    fprintf(stderr, "    -e [EMAIL]\tYour email address (required)\n");
     fprintf(stderr, "    -h or -?\tShow the help text\n");
-    fprintf(stderr, "    -H [HostID]\tAttach a custom HostID to the submission.\n");
-    fprintf(stderr, "    -i [FILE]\tSubmit an \"interesting\" file\n");
     fprintf(stderr, "    -n [FILE]\tSubmit a false negative (FN)\n");
+    fprintf(stderr, "    -N [NAME]\tYour name (required)\n");
     fprintf(stderr, "    -p [FILE]\tSubmit a fase positive (FP)\n");
     exit(0);
 }
@@ -30,65 +30,58 @@ int main(int argc, char *argv[])
     struct curl_httppost *post=NULL, *last=NULL;
     struct curl_slist *slist = NULL;
     char *type;
-    char *hostid=NULL;
+    char *name=NULL, *email=NULL;
     struct cl_engine *engine;
 
     while ((ch = my_getopt(argc, argv, OPTS)) > 0) {
-        if (ch == 'H')
-            hostid = optarg;
+        switch (ch) {
+            case 'e':
+                email = optarg;
+                break;
+            case 'N':
+                name = optarg;
+                break;
+            case 'h':
+            case '?':
+                usage(argv[0]);
+        }
     }
+
+    if (!(name) || !(email))
+        usage(argv[0]);
 
     /* Reset getopt */
     optind = opterr = 1;
     optopt = 0;
     optarg = NULL;
 
-    if (!(hostid)) {
-        cl_init(0);
-        engine = cl_engine_new();
-        if (!(engine)) {
-            fprintf(stderr, "Unable to create new ClamAV engine\n");
-            return 1;
-        }
-
-        hostid = engine->cb_stats_get_hostid(engine->stats_data);
-    }
-
     curl_global_init(CURL_GLOBAL_ALL);
 
     curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, "http://stats.clamav.dev:8080/clamav/1/submit/file");
 
     slist = curl_slist_append(slist, "Expect:");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
 
     while ((ch = my_getopt(argc, argv, OPTS)) > 0) {
-        if (ch == 'H')
+        if (ch == 'p')
+            curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendfp.cgi");
+        else if (ch == 'n')
+            curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendmalware.cgi");
+        else
             continue;
-
-        switch (ch) {
-            case 'p':
-                type = "fp";
-                break;
-            case 'n':
-                type = "fn";
-                break;
-            case 'i':
-                type = "interesting";
-                break;
-            case '?':
-            case 'h':
-            default:
-                usage(argv[0]);
-        }
 
         if ((post))
             curl_formfree(post);
 
         post = last = NULL;
 
-        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "type", CURLFORM_COPYCONTENTS, type, CURLFORM_END)) {
-            fprintf(stderr, "Unable to specify type of file in libcurl form for file %s\n", optarg);
+        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "sendername", CURLFORM_COPYCONTENTS, name, CURLFORM_END)) {
+            fprintf(stderr, "Unable to specify name in libcurl form for file %s\n", optarg);
+            break;
+        }
+
+        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "email", CURLFORM_COPYCONTENTS, email, CURLFORM_END)) {
+            fprintf(stderr, "Unable to specify email in libcurl form for file %s\n", optarg);
             break;
         }
 
@@ -97,10 +90,9 @@ int main(int argc, char *argv[])
             break;
         }
 
-        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "hostid", CURLFORM_COPYCONTENTS, hostid, CURLFORM_END)) {
-            fprintf(stderr, "Unable to specify HostID in libcurl form for file %s\n", optarg);
-            break;
-        }
+        curl_formadd(&post, &last, CURLFORM_COPYNAME, "action", CURLFORM_COPYCONTENTS, "submit", CURLFORM_END);
+        curl_formadd(&post, &last, CURLFORM_COPYNAME, "privacy", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
+        curl_formadd(&post, &last, CURLFORM_COPYNAME, "notify", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
 
         curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
         res = curl_easy_perform(curl);
