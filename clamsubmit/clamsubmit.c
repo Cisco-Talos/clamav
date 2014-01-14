9cd8d3f7ca4f5e0f634a3b46defa2add7aa81385
Shawn Webb <swebb@sourcefire.com>
2014-01-14 11:43:49 -0500

    Support streaming from stdin

--- clamsubmit/clamsubmit.c
+++ clamsubmit/clamsubmit.c
@@ -10,6 +10,8 @@
 
 #define OPTS "e:p:n:N:H:h?"
 
+char *read_stream(void);
+
 void usage(char *name)
 {
     fprintf(stderr, "USAGE: %s -hHinp?\n", name);
@@ -19,6 +21,7 @@ void usage(char *name)
     fprintf(stderr, "    -n [FILE]\tSubmit a false negative (FN)\n");
     fprintf(stderr, "    -N [NAME]\tYour name (required)\n");
     fprintf(stderr, "    -p [FILE]\tSubmit a fase positive (FP)\n");
+    fprintf(stderr, "You must specify -n or -p. Both are mutually exclusive. Pass in - as the filename for stdin.\n");
     exit(0);
 }
 
@@ -30,8 +33,17 @@ int main(int argc, char *argv[])
     struct curl_httppost *post=NULL, *last=NULL;
     struct curl_slist *slist = NULL;
     char *type;
-    char *name=NULL, *email=NULL;
+    char *name=NULL, *email=NULL, *filename=NULL;
     struct cl_engine *engine;
+    int setURL=0, fromStream=0;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    curl = curl_easy_init();
+    if (!(curl)) {
+        fprintf(stderr, "ERROR: Could not initialize libcurl\n");
+        exit(1);
+    }
 
     while ((ch = my_getopt(argc, argv, OPTS)) > 0) {
         switch (ch) {
@@ -41,68 +53,126 @@ int main(int argc, char *argv[])
             case 'N':
                 name = optarg;
                 break;
+            case 'p':
+                if (setURL)
+                    usage(argv[0]);
+
+                filename = optarg;
+
+                curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendfp.cgi");
+                setURL=1;
+                break;
+            case 'n':
+                if (setURL)
+                    usage(argv[0]);
+
+                filename = optarg;
+
+                curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendmalware.cgi");
+                setURL=1;
+                break;
             case 'h':
             case '?':
+            default:
                 usage(argv[0]);
         }
     }
 
-    if (!(name) || !(email))
+    if (!(name) || !(email) || !(filename))
         usage(argv[0]);
 
-    /* Reset getopt */
-    optind = opterr = 1;
-    optopt = 0;
-    optarg = NULL;
+    if (strlen(filename) == 1 && filename[0] == '-') {
+        filename = read_stream();
+        if (!(filename)) {
+            fprintf(stderr, "ERROR: Unable to read stream\n");
+            exit(1);
+        }
 
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    curl = curl_easy_init();
+        fromStream=1;
+    }
 
     slist = curl_slist_append(slist, "Expect:");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
 
-    while ((ch = my_getopt(argc, argv, OPTS)) > 0) {
-        if (ch == 'p')
-            curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendfp.cgi");
-        else if (ch == 'n')
-            curl_easy_setopt(curl, CURLOPT_URL, "http://cgi.clamav.net/sendmalware.cgi");
-        else
-            continue;
+    if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "sendername", CURLFORM_COPYCONTENTS, name, CURLFORM_END)) {
+        fprintf(stderr, "Unable to specify name in libcurl form for file %s\n", optarg);
+        goto end;
+    }
 
-        if ((post))
-            curl_formfree(post);
+    if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "email", CURLFORM_COPYCONTENTS, email, CURLFORM_END)) {
+        fprintf(stderr, "Unable to specify email in libcurl form for file %s\n", optarg);
+        goto end;
+    }
 
-        post = last = NULL;
+    if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "file", CURLFORM_FILE, filename, CURLFORM_END)) {
+        fprintf(stderr, "Unable to specify file path in libcurl form for file %s\n", optarg);
+        goto end;
+    }
 
-        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "sendername", CURLFORM_COPYCONTENTS, name, CURLFORM_END)) {
-            fprintf(stderr, "Unable to specify name in libcurl form for file %s\n", optarg);
-            break;
+    curl_formadd(&post, &last, CURLFORM_COPYNAME, "action", CURLFORM_COPYCONTENTS, "submit", CURLFORM_END);
+    curl_formadd(&post, &last, CURLFORM_COPYNAME, "privacy", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
+    curl_formadd(&post, &last, CURLFORM_COPYNAME, "notify", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
+
+    curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
+    res = curl_easy_perform(curl);
+
+    if (res) {
+        fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
+    }
+
+end:
+    curl_easy_cleanup(curl);
+    if (fromStream) {
+        remove(filename);
+        free(filename);
+    }
+
+    return 0;
+}
+
+char *read_stream(void)
+{
+    char *filename;
+    char buf[512];
+    size_t nread, nwritten;
+    FILE *fp;
+
+    filename = cli_gentemp(NULL);
+    if (!(filename)) {
+        return NULL;
+    }
+
+    fp = fopen(filename, "w");
+    if (!(fp)) {
+        free(filename);
+        return NULL;
+    }
+
+    while (!feof(stdin)) {
+        nwritten = 0;
+        nread = fread(buf, 1, sizeof(buf), stdin);
+        if (nread == 0) {
+            fclose(fp);
+            remove(filename);
+            free(filename);
+            return NULL;
         }
 
-        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "email", CURLFORM_COPYCONTENTS, email, CURLFORM_END)) {
-            fprintf(stderr, "Unable to specify email in libcurl form for file %s\n", optarg);
-            break;
-        }
+        while (nwritten < nread) {
+            size_t i;
+            i = fwrite(buf, 1, nread, fp);
+            if (i == 0) {
+                fclose(fp);
+                remove(filename);
+                free(filename);
+                return NULL;
+            }
 
-        if (curl_formadd(&post, &last, CURLFORM_COPYNAME, "file", CURLFORM_FILE, optarg, CURLFORM_END)) {
-            fprintf(stderr, "Unable to specify file path in libcurl form for file %s\n", optarg);
-            break;
-        }
-
-        curl_formadd(&post, &last, CURLFORM_COPYNAME, "action", CURLFORM_COPYCONTENTS, "submit", CURLFORM_END);
-        curl_formadd(&post, &last, CURLFORM_COPYNAME, "privacy", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
-        curl_formadd(&post, &last, CURLFORM_COPYNAME, "notify", CURLFORM_COPYCONTENTS, "yes", CURLFORM_END);
-
-        curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
-        res = curl_easy_perform(curl);
-
-        if (res) {
-            fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
+            nwritten += i;
         }
     }
 
-    curl_easy_cleanup(curl);
+    fclose(fp);
 
-    return 0;
+    return filename;
 }
