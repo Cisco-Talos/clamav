ccc94d90038649747f96cd2cfee065c2551f5387
Kevin Lin <klin@sourcefire.com>
2014-06-20 16:17:00 -0400

    added kfreebsd* to target OS hardcoded embedded llvm version to 2.8 changed supported system-llvm 2.9 -> 3.4.1 minor compilation changes

--- libclamav/c++/bytecode2llvm.cpp
+++ libclamav/c++/bytecode2llvm.cpp
@@ -177,10 +177,6 @@ void LLVMInitializePowerPCAsmPrinter();
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-extern "C" {
-#include "libclamav/crypto.h"
-}
-
 #include "dconf.h"
 #include "clamav.h"
 #include "clambc.h"
