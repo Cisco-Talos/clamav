fb05a793bc83c431456d94b0cac7f18a69a439c0
Shawn Webb <swebb@sourcefire.com>
2014-08-26 13:47:27 -0400

    Use a minimum set of flags when dealing with XML data

--- libclamav/dmg.c
+++ libclamav/dmg.c
@@ -209,7 +209,7 @@ int cli_scandmg(cli_ctx *ctx)
 #if HAVE_LIBXML2
 
 /* XML_PARSE_NOENT | XML_PARSE_NONET | XML_PARSE_COMPACT */
-#define DMG_XML_PARSE_OPTS (1 << 1 | 1 << 11 | 1 << 16)
+#define DMG_XML_PARSE_OPTS ((1 << 1 | 1 << 11 | 1 << 16) | CLAMAV_MIN_XMLREADER_FLAGS)
 
     reader = xmlReaderForMemory(outdata, (int)hdr.xmlLength, "toc.xml", NULL, DMG_XML_PARSE_OPTS);
     if (!reader) {
