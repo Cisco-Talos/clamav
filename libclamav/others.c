7af020b27c75c092f648b653d091d19b3758105f
Kevin Lin <klin@sourcefire.com>
2014-09-16 12:08:16 -0400

    fixed undefined reference if pcre is disabled

--- libclamav/others.c
+++ libclamav/others.c
@@ -429,7 +429,9 @@ struct cl_engine *cl_engine_new(void)
     new->maxiconspe = CLI_DEFAULT_MAXICONSPE;
 
     /* PCRE matching limitations */
+#if HAVE_PCRE
     cli_pcre_init();
+#endif
     new->pcre_match_limit = CLI_DEFAULT_PCRE_MATCH_LIMIT;
     new->pcre_recmatch_limit = CLI_DEFAULT_PCRE_RECMATCH_LIMIT;
 
