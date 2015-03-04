0945e3c5f1c99ebd22f5826f63b0b50c45a539d1
Kevin Lin <klin@sourcefire.com>
2015-03-04 14:04:24 -0500

    updated example fileprop analysis bytecodes moved old example bytecodes to examples/fileprop_analysis/old/

--- examples/fileprop_analysis/sandbox.c
+++ examples/fileprop_analysis/sandbox.c
@@ -1,26 +1,15 @@
 VIRUSNAME_PREFIX("SUBMIT")
 VIRUSNAMES("Sandbox")
 
-/* Target type is 13, internal JSON properties */
-TARGET(13)
+/* Target type is 0, all relevant files */
+TARGET(0)
+
+/* Declares to run bytecode only for preclassification (affecting only preclass files) */
+PRECLASS_HOOK_DECLARE
 
 /* JSON API call will require FUNC_LEVEL_098_5 = 78 */
-FUNCTIONALITY_LEVEL_MIN(FUNC_LEVEL_098_5)
-
-SIGNATURES_DECL_BEGIN
-DECLARE_SIGNATURE(sig1)
-SIGNATURES_DECL_END
-
-SIGNATURES_DEF_BEGIN
-/* search @offset 0 : '{ "Magic": "CLAMJSON' */
-/* this can be readjusted for specific filetypes */
-DEFINE_SIGNATURE(sig1, "0:7b20224d61676963223a2022434c414d4a534f4e")
-SIGNATURES_END
-
-bool logical_trigger(void)
-{
-    return matches(Signatures.sig1);
-}
+/* PRECLASS_HOOK_DECLARE will require FUNC_LEVEL_098_7 = 80 */
+FUNCTIONALITY_LEVEL_MIN(FUNC_LEVEL_098_7)
 
 int entrypoint ()
 {
