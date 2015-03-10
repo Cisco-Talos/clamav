a7ad8e7a31870d563c869f23fadca700eee410f3
Kevin Lin <klin@sourcefire.com>
2015-03-10 14:17:48 -0400

    added new source files to handle msxml files

---
+++ libclamav/msxml.c
@@ -0,0 +1,28 @@
+/*
+ * Extract component parts of MS XML files (e.g. MS Office 2003 XML Documents)
+ * 
+ * Copyright (C) 2007-2013 Sourcefire, Inc.
+ * 
+ * Authors: Kevin Lin
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "clamav.h"
+#include "others.h"
+
+int scan_msxml(cli_ctx *ctx)
+{
+    return CL_SUCCESS;
+}
