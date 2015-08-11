a96da1a1f2d5420c7e86440a26339df8c35f5204
Kevin Lin <klin@sourcefire.com>
2015-08-11 17:12:15 -0400

    dconf: disable swizzor heuristic by default

--- libclamav/dconf.c
+++ libclamav/dconf.c
@@ -60,7 +60,7 @@ static struct dconf_module modules[] = {
     { "PE",     "MD5SECT",      PE_CONF_MD5SECT,        1 },
     { "PE",     "UPX",      PE_CONF_UPX,        1 },
     { "PE",     "FSG",      PE_CONF_FSG,        1 },
-    { "PE",     "SWIZZOR",      PE_CONF_SWIZZOR,        1 },
+    { "PE",     "SWIZZOR",      PE_CONF_SWIZZOR,        0 },
 
     { "PE",     "PETITE",       PE_CONF_PETITE,     1 },
     { "PE",     "PESPIN",       PE_CONF_PESPIN,     1 },
