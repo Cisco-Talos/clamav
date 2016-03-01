1936d3bf331b04f399d55fb400585526e4ea5d11
Kevin Lin <klin@sourcefire.com>
2016-03-01 12:33:54 -0500

    whitespace fix

--- libclamav/scanners.c
+++ libclamav/scanners.c
@@ -2699,7 +2699,7 @@ static int magic_scandesc(cli_ctx *ctx, cli_file_t type)
                 type == CL_TYPE_HWP3 ||
                 type == CL_TYPE_XML_HWP ||
                 type == CL_TYPE_HWPOLE2 ||
-		type == CL_TYPE_OOXML_HWP) {
+                type == CL_TYPE_OOXML_HWP) {
                 ctx->properties = json_object_new_object();
                 if (NULL == ctx->properties) {
                     cli_errmsg("magic_scandesc: no memory for json properties object\n");
