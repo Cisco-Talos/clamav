e7facb06d78670dcdbe95bae9de9b9ac50fc5ff1
Kevin Lin <klin@sourcefire.com>
2016-04-18 17:11:12 -0400

    pdfdecode: do not apply forced decryption to /XRef streams

--- libclamav/pdfdecode.h
+++ libclamav/pdfdecode.h
@@ -36,6 +36,6 @@
 
 #include "pdf.h"
 
-off_t pdf_decodestream(struct pdf_struct *pdf, struct pdf_obj *obj, struct pdf_dict *params, const char *stream, uint32_t streamlen, int fout, int *rc);
+off_t pdf_decodestream(struct pdf_struct *pdf, struct pdf_obj *obj, struct pdf_dict *params, const char *stream, uint32_t streamlen, int xref, int fout, int *rc);
 
 #endif /* __PDFDECODE_H__ */
