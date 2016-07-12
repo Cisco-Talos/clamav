f612338bd6997faad6a0f2ea5c0a686707680b72
Steven Morgan <smorgan@sourcefire.com>
2016-07-12 12:36:29 -0400

    bb11600 - fix out of bounds stack read.

--- libclamunrar/unrar20.c
+++ libclamunrar/unrar20.c
@@ -117,7 +117,8 @@ static int read_tables20(int fd, unpack_data_t *unpack_data)
 			n = (rar_getbits(unpack_data) >> 14) + 3;
 			rar_addbits(unpack_data, 2);
 			while ((n-- > 0) && (i < table_size)) {
-				table[i] = table[i-1];
+				if (i>0)
+					table[i] = table[i-1];
 				i++;
 			}
 		} else {
