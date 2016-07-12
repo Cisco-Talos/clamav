64f9b9b5dd5a8b75fdba9820c88ecfcf36ae98ca
Steven Morgan <smorgan@sourcefire.com>
2016-07-12 14:31:38 -0400

    fix possible out of bounds stack read.

--- libclamunrar/unrar.c
+++ libclamunrar/unrar.c
@@ -469,7 +469,8 @@ static int read_tables(int fd, unpack_data_t *unpack_data)
 				rar_addbits(unpack_data, 7);
 			}
 			while (n-- > 0 && i < table_size) {
-				table[i] = table[i-1];
+				if (i>0)
+					table[i] = table[i-1];
 				i++;
 			}
 		} else {
