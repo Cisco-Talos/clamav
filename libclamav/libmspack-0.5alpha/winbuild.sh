422e628b866bd157915b9c1dd165a9913e49094b
Mickey Sola <msola@sourcefire.com>
2016-09-08 13:04:43 -0400

    bb11062 - updating libmspack windows installer to automate clamdep setup

--- libclamav/libmspack-0.5alpha/winbuild.sh
+++ libclamav/libmspack-0.5alpha/winbuild.sh
@@ -3,8 +3,6 @@ cat <<END
 !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! 
 This builds the libmspack into mspack.dll on
 Windows with Microsoft compiler.
-After compilation find the library in the 
-directory mspack 
 !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! !!! 
 END
 
@@ -15,12 +13,58 @@ cat >config.h <<END
 #define HAVE_MEMCMP 1
 END
 
-cd mspack
+# Change if your VS 2015 path differs.
+VS_PATH="C:\Program Files\Microsoft Visual Studio 14.0\VC"
 
-cl /O2 -I. /c *.c
-link *.obj /DLL /DEF:mspack.def /IMPLIB:mspack.lib
+# Do not change anything below unless you know what you're doing.
+LIBMS_PATH=`pwd`
+LIBMS_PATH+='/mspack'
 
-cd ..
+compile_for () {
+    echo "Configuring windows compiler for $1 ..."
 
-ls -l mspack/mspack.dll
-ls -l mspack/mspack.lib
+    cd "${VS_PATH}"
+    ./vcvarsall.bat $1
+
+    cd "${LIBMS_PATH}"
+    cl /O2 -I. /c *.c
+    link *.obj /DLL /DEF:mspack.def /IMPLIB:mspack.lib
+
+    cd ..
+
+    echo 'Checking for dll and lib ...'
+    ls -l mspack/mspack.dll
+    ls -l mspack/mspack.lib
+
+    echo 'Copying over dll and lib ...'
+    cp mspack/mspack.dll "C:\\clamdeps\\win$2\\mspack\\lib"
+    cp mspack/mspack.lib "C:\\clamdeps\\win$2\\mspack\\lib"
+}
+
+copy_lib () {
+    echo 'Copying headers and source files ...'
+
+    cd ${LIBMS_PATH}
+    cp *.c "C:\\clamdeps\\win$1\\mspack\\include"
+    cp *.h "C:\\clamdeps\\win$1\\mspack\\include"
+}
+
+if [[ -d 'C:\clamdeps\win32\mspack\include' && -d 'C:\clamdeps\win32\mspack\lib' && -d 'C:\clamdeps\win64\mspack\include' && -d 'C:\clamdeps\win64\mspack\lib' ]]; then
+
+    copy_lib 32
+    copy_lib 64
+
+    if [ $(uname -m) == 'x86_64' ]; then
+        compile_for amd64_x86 32
+        compile_for amd64 64
+    else
+        compile_for x86 32
+        compile_for x86_amd64 64
+
+    fi
+else
+    echo 'ERROR: C:\clamdeps\winXX\mspack\include and/or C:\clamdeps\winXX\mspack\lib not found.'
+fi
+
+
+
