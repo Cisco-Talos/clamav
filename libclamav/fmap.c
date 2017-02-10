bef16f47308374c4b367523a06997506a4709a82
Steven Morgan <stevmorg@cisco.com>
2017-02-10 12:59:23 -0500

    bb11743 - fix compiler warning. Patch contributed by Ruga.

--- libclamav/fmap.c
+++ libclamav/fmap.c
@@ -547,7 +547,7 @@ static void unmap_mmap(fmap_t *m)
     size_t len = m->pages * m->pgsz + m->hdrsz;
     fmap_lock;
     if (munmap((void *)m, len) == -1) /* munmap() failed */
-        cli_warnmsg("funmap: unable to unmap memory segment at address: %p with length: %d\n", (void *)m, len);
+        cli_warnmsg("funmap: unable to unmap memory segment at address: %p with length: %zu\n", (void *)m, len);
     fmap_unlock;
 #endif
 }
