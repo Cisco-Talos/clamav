56ad1622f6e00fc7ccbace8a72cd009df4f685c1
Micah Snyder <micasnyd@cisco.com>
2017-08-09 14:28:47 -0400

    updating the copyright for a number of tiles.

--- clamav-milter/clamav-milter.c
+++ clamav-milter/clamav-milter.c
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2015 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
- *  Copyright (C)2008 Sourcefire, Inc.
+ *  Copyright (C) 2008 Sourcefire, Inc.
  *
  *  Author: aCaB <acab@clamav.net>
  *
