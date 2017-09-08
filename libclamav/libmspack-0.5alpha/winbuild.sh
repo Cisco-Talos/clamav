c2cfda510e6d7d9cdc00841abc309870efbc5ec1
Micah Snyder <micasnyd@cisco.com>
2017-09-08 15:50:46 -0400

    partial pull-request from jfehr, ommiting changes to openssl lib names: Changes needed to build Cmam in VS2015.

--- libclamav/libmspack-0.5alpha/winbuild.sh
+++ libclamav/libmspack-0.5alpha/winbuild.sh
@@ -14,7 +14,12 @@ cat >config.h <<END
 END
 
 # Change if your VS 2015 path differs.
-VS_PATH='C:\Program Files\Microsoft Visual Studio 14.0\VC'
+if [ $(uname -m) == 'x86_64' ]; then
+	VS_PATH='C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC'
+else
+	VS_PATH='C:\Program Files\Microsoft Visual Studio 14.0\VC'
+fi
+
 
 # Do not change anything below unless you know what you're doing.
 LIBMS_PATH=`pwd`
@@ -36,28 +41,37 @@ compile_for () {
     cp mspack/mspack.lib "C:\\clamdeps\\win$2\\mspack\\lib"
 }
 
-copy_lib () {
+copy_headers () {
     echo 'Copying headers and source files ...'
 
     cd ${LIBMS_PATH}
-    cp *.c "C:\\clamdeps\\win$1\\mspack\\include"
     cp *.h "C:\\clamdeps\\win$1\\mspack\\include"
 }
 
-if [[ -d 'C:\clamdeps\win32\mspack\include' && -d 'C:\clamdeps\win32\mspack\lib' && -d 'C:\clamdeps\win64\mspack\include' && -d 'C:\clamdeps\win64\mspack\lib' ]]; then
-
-    copy_lib 32
-    copy_lib 64
-
-    if [ $(uname -m) == 'x86_64' ]; then
-        compile_for 'amd64_x86' 32
-        compile_for 'amd64' 64
-    else
-        compile_for 'x86' 32
-        compile_for 'x86_amd64' 64
-
-    fi
-else
-    echo 'ERROR: mspack include and/or lib clamdep dirs not found'
+if ! [ -d 'C:\clamdeps\win32\mspack\include' ]; then
+	mkdir -p 'C:\clamdeps\win32\mspack\include'
 fi
 
+if ! [ -d 'C:\clamdeps\win32\mspack\lib' ]; then
+	mkdir -p 'C:\clamdeps\win32\mspack\lib'
+fi
+
+if ! [ -d 'C:\clamdeps\win64\mspack\include' ]; then
+	mkdir -p 'C:\clamdeps\win64\mspack\include'
+fi
+
+if ! [ -d 'C:\clamdeps\win64\mspack\lib' ]; then
+	mkdir -p 'C:\clamdeps\win64\mspack\lib'
+fi
+
+copy_headers 32
+copy_headers 64
+
+if [ $(uname -m) == 'x86_64' ]; then
+    compile_for 'amd64_x86' 32
+    compile_for 'amd64' 64
+else
+    compile_for 'x86' 32
+    compile_for 'x86_amd64' 64
+
+fi
