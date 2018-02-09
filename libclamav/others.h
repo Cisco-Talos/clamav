6bac17ac99d7cca6668027a93da2715b6a320f13
Micah Snyder <micasnyd@cisco.com>
2018-02-09 18:48:54 -0500

    Preparing for 0.99.4 by updating version number, flevel, readme, news. Additional changes may be needed prior to release.

--- libclamav/others.h
+++ libclamav/others.h
@@ -72,7 +72,7 @@
  * in re-enabling affected modules.
  */
 
-#define CL_FLEVEL 84
+#define CL_FLEVEL 85
 #define CL_FLEVEL_DCONF	CL_FLEVEL
 #define CL_FLEVEL_SIGTOOL CL_FLEVEL
 
--- m4/reorganization/version.m4
+++ m4/reorganization/version.m4
@@ -1,6 +1,6 @@
 dnl change this on a release
 dnl VERSION="devel-`date +%Y%m%d`"
-VERSION="0.99.3"
+VERSION="0.99.4"
 
 LC_CURRENT=8
 LC_REVISION=1
