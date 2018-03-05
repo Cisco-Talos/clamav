c651294fb6612641d74ed6054bea6f3d1ea89e55
Micah Snyder <micasnyd@cisco.com>
2018-03-05 14:49:09 -0500

    Changing version string to 0.100.0-rc

--- win32/clamav-config.h
+++ win32/clamav-config.h
@@ -511,7 +511,7 @@
 /* #undef USE_SYSLOG */
 
 /* Version number of package */
-#define VERSION "devel-clamav-0.97-408-ge11f7cc"
+#define VERSION "devel-clamav-0.100.0-rc"
 
 /* Version suffix for package */
 #define VERSION_SUFFIX ""
