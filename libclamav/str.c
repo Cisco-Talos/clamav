ef9239d5bebb07493f2b9fbb077ccb9eaeaa1acd
Micah Snyder <micasnyd@cisco.com>
2018-06-01 14:23:25 -0400

    bb12133: Implementing cli_strntol based on gnu gcc's strtol implementation with modifications to limit string buffer length for non-null terminated strings. Using cli_strntol in pdf.c for added safety.

--- libclamav/str.c
+++ libclamav/str.c
@@ -516,6 +516,121 @@ size_t cli_strtokenize(char *buffer, const char delim, const size_t token_count,
     return tokens_found;
 }
 
+/**
+ * @brief The strntol() function converts the string in str to a long value.
+ * Modifications made to validate the length of the string for non-null term strings.
+ *
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * @param nptr 		Pointer to start of string.
+ * @param n 		Max length of buffer in bytes.
+ * @param endptr 	[OUT optional] If endptr is not NULL, strtol() stores the address
+ * 					of the first invalid character in *endptr. If there were no digits
+ * 					at all, however, strtol() stores the
+ * 					original value of str in *endptr.
+ * @param int 		The conversion is done according to the given base, which must be
+ * 					between 2 and 36 inclusive, or be the special value 0.
+ * @return long 	The signed long value.
+ */
+long cli_strntol(const char *nptr, size_t n, char **endptr, register int base)
+{
+	register const char *s = nptr;
+	register unsigned long acc;
+	register int c;
+	register unsigned long cutoff;
+	register int neg = 0, any, cutlim;
+
+    if (0 == n) {
+        return 0;
+    }
+	/*
+	 * Skip white space and pick up leading +/- sign if any.
+	 * If base is 0, allow 0x for hex and 0 for octal, else
+	 * assume decimal; if base is already 16, allow 0x.
+	 */
+	do {
+		c = *s;
+	} while (isspace(c) && (++s < nptr + n));
+
+    if (s >= nptr + n) {
+        return 0;
+    }
+
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+        if (s >= nptr + n)
+            return 0;
+	} else if (c == '+') {
+		c = *s++;
+        if (s >= nptr + n)
+            return 0;
+    }
+
+	if (base == 0 || base == 16) {
+        if (c == '0' && (*s == 'x' || *s == 'X')) {
+            if (s + 2 >= nptr + n) {
+                return 0;
+            }
+            c = s[1];
+            s += 2;
+            base = 16;
+        }
+	}
+
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
+
+	/*
+	 * Compute the cutoff value between legal numbers and illegal
+	 * numbers.  That is the largest legal value, divided by the
+	 * base.  An input number that is greater than this value, if
+	 * followed by a legal input character, is too big.  One that
+	 * is equal to this value may be valid or not; the limit
+	 * between valid and invalid numbers is then based on the last
+	 * digit.  For instance, if the range for longs is
+	 * [-2147483648..2147483647] and the input base is 10,
+	 * cutoff will be set to 214748364 and cutlim to either
+	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
+	 * the number is too big, and we will return a range error.
+	 *
+	 * Set any if any `digits' consumed; make it negative to indicate
+	 * overflow.
+	 */
+	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
+	cutlim = cutoff % (unsigned long)base;
+	cutoff /= (unsigned long)base;
+	for (acc = 0, any = 0; s < nptr + n; s++) {
+        c = *s;
+
+		if (isdigit(c))
+			c -= '0';
+		else if (isalpha(c))
+			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+			any = -1;
+		else {
+			any = 1;
+			acc *= base;
+			acc += c;
+		}
+	}
+	if (any < 0) {
+		acc = neg ? LONG_MIN : LONG_MAX;
+		errno = ERANGE;
+	} else if (neg)
+		acc = -acc;
+	if (endptr != 0)
+		*endptr = (char *) (any ? s - 1 : nptr);
+	return (acc);
+}
+
 size_t cli_ldbtokenize(char *buffer, const char delim, const size_t token_count, const char **tokens, int token_skip)
 {
     size_t tokens_found, i;
