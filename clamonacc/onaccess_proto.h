2f45e13f9ecbef2eb58971ba20ab0c2c0a552983
Mickey Sola <msola@sourcefire.com>
2018-12-17 11:47:18 -0500

    clamonacc - make clamonacc connect with clamd

---
+++ clamonacc/onaccess_proto.h
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (C) 2015 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+ *  Copyright (C) 2009 Sourcefire, Inc.
+ *
+ *  Authors: Tomasz Kojm, aCaB
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+
+#ifndef ONAS_PROTO_H
+#define ONAS_PROTO_H
+#include "shared/misc.h"
+
+int dconnect(void);
+int serial_client_scan(char *file, int scantype, int *infected, int *err, int maxlevel, int flags);
+int parallel_client_scan(char *file, int scantype, int *infected, int *err, int maxlevel, int flags);
+int dsresult(int sockd, int scantype, const char *filename, int *printok, int *errors);
+#endif
