be2a91cc56e0fb19e190b11aad129ddb303d15ab
Micah Snyder (micasnyd) <micasnyd@cisco.com>
2020-08-24 15:11:07 -0700

    bb12596: Fix build issue when libcheck missing
    If libcheck is missing, the unit tests are skipped. Header include path
    changes mean clamav.h is not available when the unit tests are skipped,
    and thus the check_clamav_skip.c files won't compile. This patch
    removes the clamav.h dependency so `make check` will "work".

--- unit_tests/check_clamav_skip.c
+++ unit_tests/check_clamav_skip.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-// libclamav
-#include "clamav.h"
-
 int main(int argc, char **argv)
 {
-    UNUSEDPARAM(argc);
-    UNUSEDPARAM(argv);
+    (void)argc;
+    (void)argv;
     puts("\n*** Unit tests disabled in this build\n*** Use ./configure --enable-check to enable them\n");
     /* tell automake the test was skipped */
     return 77;
