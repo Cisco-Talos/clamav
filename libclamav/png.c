e37cbdffa9a13581821d00c4869fe33b1755e78a
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-02-05 18:01:42 +0300

    Update png.c

--- libclamav/png.c
+++ libclamav/png.c
@@ -424,7 +424,7 @@ scan_overlay:
     }
 
     /* Check if there's an overlay, and scan it if one exists. */
-    if (map->len - offset > 0) {
+    if (map->len > offset) {
         cli_dbgmsg("PNG: Found " STDu64 " additional data after end of PNG! Scanning as a nested file.\n", map->len - offset);
         status = cli_magic_scan_nested_fmap_type(map, (size_t)offset, map->len - offset, ctx, CL_TYPE_ANY, NULL);
         goto done;
