6096605cc63b97d7e84e1c313d0d26d75b086112
Micah Snyder <micasnyd@cisco.com>
2021-04-08 12:03:54 -0700

    CMake: Fix Windows build, UnRAR dll extension
    The previous UnRAR vuln fix worked for the (old, gone) Visual Studio
    solution but broke the CMake build.  We didn't notice because we tested
    and approved it with Visual Studio before switching to CMake.
    
    This change switches it to use the correct macro for the
    libclamunrar_iface extension which is ".dll" instead of "lib".

--- libclamav/others.c
+++ libclamav/others.c
@@ -172,13 +172,15 @@ static void *get_module_function(lt_dlhandle handle, const char *name)
 
 static void *load_module(const char *name, const char *featurename)
 {
+#ifdef _WIN32
+    static const char *suffixes[] = {LT_MODULE_EXT};
+#else
     static const char *suffixes[] = {
-#ifndef _WIN32
         LT_MODULE_EXT "." LIBCLAMAV_FULLVER,
         PASTE(LT_MODULE_EXT ".", LIBCLAMAV_MAJORVER),
         LT_MODULE_EXT,
-#endif
         "." LT_LIBEXT};
+#endif
 
     const char *searchpath;
     char modulename[128];
