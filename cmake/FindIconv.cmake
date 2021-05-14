fe96de86bb90c489aa509ee9135f776b7a2a7eb4
Micah Snyder (micasnyd) <micasnyd@cisco.com>
2021-05-14 11:57:28 -0700

    CMake: revert Iconv module name to match upstream
    I'd foolishly set the Iconv detection module to use uppercase to
    differentiate it from the upstream (original) module. I also kind of
    forgot to change the documentation to use the uppercase name. In
    reviewing that, I decided it's better to match the upstream
    FindIconv.cmake module as closely as possible in case they fix the issue
    described here, so that we can stop carrying a custom module without
    breaking the CMake options for everyone:
    https://github.com/Cisco-Talos/clamav-devel/commit/b26cdc54532729ecbc5e48dce51534eeb9c5497c
    
    This reverts the CMake options for setting the Iconv include and library
    paths back to match the upstream before 0.104 ships with stable CMake
    support.

--- cmake/FindIconv.cmake
+++ cmake/FindIconv.cmake
@@ -6,7 +6,7 @@
 # Mods by Micah Snyder to support systems with both libc's iconv + libconv
 
 #[=======================================================================[.rst:
-FindICONV
+FindIconv
 ---------
 
 .. versionadded:: 3.11
@@ -17,19 +17,19 @@ in the form of an additional library.
 
 The following variables are provided to indicate iconv support:
 
-.. variable:: ICONV_FOUND
+.. variable:: Iconv_FOUND
 
   Variable indicating if the iconv support was found.
 
-.. variable:: ICONV_INCLUDE_DIRS
+.. variable:: Iconv_INCLUDE_DIRS
 
   The directories containing the iconv headers.
 
-.. variable:: ICONV_LIBRARIES
+.. variable:: Iconv_LIBRARIES
 
   The iconv libraries to be linked.
 
-.. variable:: ICONV_IS_BUILT_IN
+.. variable:: Iconv_IS_BUILT_IN
 
   A variable indicating whether iconv support is stemming from the
   C library or not. Even if the C library provides `iconv()`, the presence of
@@ -37,23 +37,23 @@ The following variables are provided to indicate iconv support:
 
 Additionally, the following :prop_tgt:`IMPORTED` target is being provided:
 
-.. variable:: ICONV::Iconv
+.. variable:: Iconv::Iconv
 
   Imported target for using iconv.
 
 The following cache variables may also be set:
 
-.. variable:: ICONV_INCLUDE_DIR
+.. variable:: Iconv_INCLUDE_DIR
 
   The directory containing the iconv headers.
 
-.. variable:: ICONV_LIBRARY
+.. variable:: Iconv_LIBRARY
 
   The iconv library (if not implicitly given in the C library).
 
 .. note::
   On POSIX platforms, iconv might be part of the C library and the cache
-  variables ``ICONV_INCLUDE_DIR`` and ``ICONV_LIBRARY`` might be empty.
+  variables ``Iconv_INCLUDE_DIR`` and ``Iconv_LIBRARY`` might be empty.
 
 #]=======================================================================]
 
@@ -63,15 +63,15 @@ include(CheckCXXSourceCompiles)
 
 # iconv can only be provided in libc on a POSIX system.
 # If any cache variable is already set, we'll skip this test.
-if(NOT DEFINED ICONV_IS_BUILT_IN)
+if(NOT DEFINED Iconv_IS_BUILT_IN)
   # Check for iconv.h first.
-  # If it's not the built-in one, then ICONV_INCLUDE_DIR will
-  find_path(ICONV_INCLUDE_DIR
+  # If it's not the built-in one, then Iconv_INCLUDE_DIR will
+  find_path(Iconv_INCLUDE_DIR
     NAMES "iconv.h"
     DOC "iconv include directory")
-  set(ICONV_LIBRARY_NAMES "iconv" "libiconv")
+  set(Iconv_LIBRARY_NAMES "iconv" "libiconv")
 
-  if(UNIX AND ICONV_INCLUDE_DIR AND NOT DEFINED ICONV_LIBRARY)
+  if(UNIX AND Iconv_INCLUDE_DIR AND NOT DEFINED Iconv_LIBRARY)
     cmake_push_check_state(RESET)
     # We always suppress the message here: Otherwise on supported systems
     # not having iconv in their C library (e.g. those using libiconv)
@@ -80,7 +80,7 @@ if(NOT DEFINED ICONV_IS_BUILT_IN)
     # The following code will not work, but it's sufficient to see if it compiles.
     # Note: libiconv will define the iconv functions as macros, so CheckSymbolExists
     # will not yield correct results.
-    set(ICONV_IMPLICIT_TEST_CODE
+    set(Iconv_IMPLICIT_TEST_CODE
       "
       #include <stddef.h>
       #include <iconv.h>
@@ -98,45 +98,45 @@ if(NOT DEFINED ICONV_IS_BUILT_IN)
     # Make sure we're using the iconv.h we found above. This way we don't
     # accidentally compile against libiconv's header later but link with only
     # libc on systems that have both (eg FreeBSD with libiconv pkg installed).
-    set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
+    set(CMAKE_REQUIRED_INCLUDES ${Iconv_INCLUDE_DIR})
 
     if(CMAKE_C_COMPILER_LOADED)
-      check_c_source_compiles("${ICONV_IMPLICIT_TEST_CODE}" ICONV_IS_BUILT_IN)
+      check_c_source_compiles("${Iconv_IMPLICIT_TEST_CODE}" Iconv_IS_BUILT_IN)
     else()
-      check_cxx_source_compiles("${ICONV_IMPLICIT_TEST_CODE}" ICONV_IS_BUILT_IN)
+      check_cxx_source_compiles("${Iconv_IMPLICIT_TEST_CODE}" Iconv_IS_BUILT_IN)
     endif()
     cmake_pop_check_state()
   else()
-    set(ICONV_IS_BUILT_IN FALSE)
+    set(Iconv_IS_BUILT_IN FALSE)
   endif()
 endif()
 
-if(ICONV_IS_BUILT_IN)
-  set(ICONV_INCLUDE_DIR "" CACHE FILEPATH "iconv include directory")
-  set(ICONV_LIBRARY_NAMES "c")
+if(Iconv_IS_BUILT_IN)
+  set(Iconv_INCLUDE_DIR "" CACHE FILEPATH "iconv include directory")
+  set(Iconv_LIBRARY_NAMES "c")
 endif()
 
-find_library(ICONV_LIBRARY
-  NAMES ${ICONV_LIBRARY_NAMES}
+find_library(Iconv_LIBRARY
+  NAMES ${Iconv_LIBRARY_NAMES}
   NAMES_PER_DIR
   DOC "iconv library (potentially the C library)")
 
-mark_as_advanced(ICONV_INCLUDE_DIR)
-mark_as_advanced(ICONV_LIBRARY)
+mark_as_advanced(Iconv_INCLUDE_DIR)
+mark_as_advanced(Iconv_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-if(NOT ICONV_IS_BUILT_IN)
-  find_package_handle_standard_args(ICONV REQUIRED_VARS ICONV_LIBRARY ICONV_INCLUDE_DIR)
+if(NOT Iconv_IS_BUILT_IN)
+  find_package_handle_standard_args(Iconv REQUIRED_VARS Iconv_LIBRARY Iconv_INCLUDE_DIR)
 else()
-  find_package_handle_standard_args(ICONV REQUIRED_VARS ICONV_LIBRARY)
+  find_package_handle_standard_args(Iconv REQUIRED_VARS Iconv_LIBRARY)
 endif()
 
-if(ICONV_FOUND)
-  set(ICONV_INCLUDE_DIRS "${ICONV_INCLUDE_DIR}")
-  set(ICONV_LIBRARIES "${ICONV_LIBRARY}")
-  if(NOT TARGET ICONV::Iconv)
-    add_library(ICONV::Iconv INTERFACE IMPORTED)
+if(Iconv_FOUND)
+  set(Iconv_INCLUDE_DIRS "${Iconv_INCLUDE_DIR}")
+  set(Iconv_LIBRARIES "${Iconv_LIBRARY}")
+  if(NOT TARGET Iconv::Iconv)
+    add_library(Iconv::Iconv INTERFACE IMPORTED)
   endif()
-  set_property(TARGET ICONV::Iconv PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${ICONV_INCLUDE_DIRS}")
-  set_property(TARGET ICONV::Iconv PROPERTY INTERFACE_LINK_LIBRARIES "${ICONV_LIBRARIES}")
+  set_property(TARGET Iconv::Iconv PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${Iconv_INCLUDE_DIRS}")
+  set_property(TARGET Iconv::Iconv PROPERTY INTERFACE_LINK_LIBRARIES "${Iconv_LIBRARIES}")
 endif()
