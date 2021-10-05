1a87e7a610ad64bb3f6d30392cd43f3909a53a21
Micah Snyder <micasnyd@cisco.com>
2021-10-05 14:26:09 -0700

    Heuristics.Email.ExceedsMax* only alert if alert-exceeds-maxi
    The Email heuristics when scan limits are exceeded should only alert if
    clamscan's `--alert-exceeds-max` option is enabled.
    
    The ClamD options is: `AlertExceedsMax`
    
    The libclamav option is: `CL_SCAN_HEURISTIC_EXCEEDS_MAX`

--- libclamav/mbox.c
+++ libclamav/mbox.c
@@ -791,7 +791,7 @@ hitLineFoldCnt(const char *const line, size_t *lineFoldCnt, cli_ctx *ctx, bool *
         }
 
         if ((*lineFoldCnt) >= HEURISTIC_EMAIL_MAX_LINE_FOLDS_PER_HEADER) {
-            if (ctx->options->general & CL_SCAN_GENERAL_HEURISTICS) {
+            if (SCAN_HEURISTIC_EXCEEDS_MAX) {
                 cli_append_virus(ctx, "Heuristics.Email.ExceedsMaxLineFoldCnt");
                 *heuristicFound = TRUE;
             }
@@ -807,7 +807,7 @@ haveTooManyHeaderBytes(size_t totalLen, cli_ctx *ctx, bool *heuristicFound)
 {
 
     if (totalLen > HEURISTIC_EMAIL_MAX_HEADER_BYTES) {
-        if (ctx->options->general & CL_SCAN_GENERAL_HEURISTICS) {
+        if (SCAN_HEURISTIC_EXCEEDS_MAX) {
             cli_append_virus(ctx, "Heuristics.Email.ExceedsMaxHeaderBytes");
             *heuristicFound = TRUE;
         }
@@ -822,7 +822,7 @@ haveTooManyEmailHeaders(size_t totalHeaderCnt, cli_ctx *ctx, bool *heuristicFoun
 {
 
     if (totalHeaderCnt > HEURISTIC_EMAIL_MAX_HEADERS) {
-        if (ctx->options->general & CL_SCAN_GENERAL_HEURISTICS) {
+        if (SCAN_HEURISTIC_EXCEEDS_MAX) {
             cli_append_virus(ctx, "Heuristics.Email.ExceedsMaxEmailHeaders");
             *heuristicFound = TRUE;
         }
@@ -837,7 +837,7 @@ haveTooManyMIMEPartsPerMessage(size_t mimePartCnt, cli_ctx *ctx, mbox_status *rc
 {
 
     if (mimePartCnt >= HEURISTIC_EMAIL_MAX_MIME_PARTS_PER_MESSAGE) {
-        if (ctx->options->general & CL_SCAN_GENERAL_HEURISTICS) {
+        if (SCAN_HEURISTIC_EXCEEDS_MAX) {
             cli_append_virus(ctx, "Heuristics.Email.ExceedsMaxMIMEPartsPerMessage");
             *rc = VIRUS;
         }
@@ -852,7 +852,7 @@ haveTooManyMIMEArguments(size_t argCnt, cli_ctx *ctx, bool *heuristicFound)
 {
 
     if (argCnt >= HEURISTIC_EMAIL_MAX_ARGUMENTS_PER_HEADER) {
-        if (ctx->options->general & CL_SCAN_GENERAL_HEURISTICS) {
+        if (SCAN_HEURISTIC_EXCEEDS_MAX) {
             cli_append_virus(ctx, "Heuristics.Email.ExceedsMaxMIMEArguments");
             *heuristicFound = TRUE;
         }
