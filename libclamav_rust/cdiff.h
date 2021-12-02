a671ff249469ce27498039fb7504ad958b7112be
John Humlick <jhumlick@cisco.com>
2021-12-02 15:07:48 -0800

    Migrate script2cdiff from sigtool.c to cdiff.rs
    getdsig was also moved from sigtool.c to dsig.c, and renamed to cli_getdsig

--- libclamav_rust/cdiff.h
+++ libclamav_rust/cdiff.h
@@ -10,6 +10,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/**
+ * # Safety
+ *
+ * This function is only meant to be called from sigtool.c
+ * If sigtool is ever ported to rust, this function should be
+ * rewritten.
+ */
+int32_t script2cdiff(const char *script, const char *builder, const char *server);
 
 int32_t cdiff_apply(int32_t file_descriptor, uint16_t mode);
 
