cd76f1f525848b4c90aae19d43ce36e7ccfb26d1
Mickey Sola <micksola@cisco.com>
2022-02-07 11:24:18 -0800

    Image fuzzy hash: initial custom phash implementation
    No longer use the Rust img_hash crate so that we can fine-tune the
    implementation to be closer to the Python imagehash package.

--- libclamav_rust/src/fuzzy_hash.rs
+++ libclamav_rust/src/fuzzy_hash.rs
@@ -20,6 +20,8 @@
  *  MA 02110-1301, USA.
  */
 
+use image::imageops::FilterType::Lanczos3;
+use rustdct::DctPlanner;
 use std::collections::HashMap;
 use std::convert::{TryFrom, TryInto};
 use std::ffi::CStr;
@@ -321,15 +323,36 @@ pub extern "C" fn _fuzzy_hash_calculate_image(
 pub fn fuzzy_hash_calculate_image(buffer: &[u8]) -> Result<Vec<u8>, FuzzyHashError> {
     let image = image::load_from_memory(buffer).map_err(|e| FuzzyHashError::ImageLoad(e.into()))?;
 
-    let hasher = img_hash::HasherConfig::new()
-        .hash_size(8, 8)
-        .resize_filter(img_hash::FilterType::Triangle)
-        // .preproc_dct()
-        .hash_alg(img_hash::HashAlg::Mean)
-        .to_hasher();
-    let hash = hasher.hash_image(&image);
+    // convert image to grayscale, then resize
+    let image_gs = image.grayscale();
 
-    let hash_bytes = hash.as_bytes();
+    let imgbuff_tmp = image::DynamicImage::resize_exact(&image_gs, 32, 32, Lanczos3).to_bytes();
+    let mut imgbuff: Vec<f32> = imgbuff_tmp.into_iter().map(|x| x as f32).collect();
+
+    // perform discrete cosine transform (dct2) on image
+    DctPlanner::new().plan_dct2(1024).process_dct2(&mut imgbuff);
+
+    // construct a low frequency dct vector using the topmost 8x8 terms
+    let mut dct_low_freq: Vec<f32> = Vec::new();
+
+    for i in 0..8 {
+        let idx = i * 32;
+        dct_low_freq.extend_from_slice(&imgbuff[idx..idx + 8]);
+    }
+
+    // take the low frequency averages, excluding the first term
+    let sum: f32 = dct_low_freq.iter().sum();
+    let sum = sum - dct_low_freq[0];
+    let mean = sum / (dct_low_freq.len() - 1) as f32;
+
+    // construct hash vector by reducing DCT values to 1 or 0 by comparing terms vs mean
+    let hashvec: Vec<u64> = dct_low_freq
+        .into_iter()
+        .map(|x| if x > mean { 1 } else { 0 })
+        .collect();
+
+    // construct hash integer from bits
+    let hash: u64 = hashvec.iter().fold(0, |res, &bit| (res << 1) ^ bit);
 
     debug!("Image hash: {:?}", hex::encode(hash_bytes));
 
