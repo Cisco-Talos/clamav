b461168117c95022bccef280e1190dfd66800330
Micah Snyder <micasnyd@cisco.com>
2022-08-17 06:48:36 -0700

    Code cleanup: More accurate variable types in bytecode modules
    The bytecode source files largely use `int` instead of the appropriate
    `cl_errot_t` for clamav status codes, as well for boolean variables.
    This hides warnings that would indicate bugs, and makes it harder to
    read the code.
    
    I haven't gone as in depth as with some other code cleanups. This
    largely just replaces function interfactes and ret variables that use
    `int` with `cl_error_t`. I also swapped a couple of `int`s to `bool`s.
    
    While doing so I found that the `cli_bytecode_context_setpdf()` function
    was incorrectly placed in the `bytecode_api.c` file instead of the next
    to similar functions (`cli_bytecode_context_setpe`, etc.) in bytecode.c.
    It's not an API function, so I moved it to the correct location.
    
    I also eliminated a couple of compiler warnings:
    
    - LLVM's CFG.h header emits a warning about a multi-line comment, so
      that crops up with using LLVM for the bytecode runtime.
      I disabled the warning through CMake.
    
    - C doesn't like using the `inline` keyword on cli_dbgmsg in the
      declaration in `bytecode2llvm.c` because we're compiling the bytecode
      runtimes as a separate object file from the rest of libclamav.
      It doesn't appear to be a functional issue, but I swapped that file
      over to use `cli_dbgmsg_no_inline()` instead, just in case.
      I would hope link-time-optimization will inline it anyways.

--- libclamav/bytecode.h
+++ libclamav/bytecode.h
@@ -90,13 +90,14 @@ struct pdf_obj;
 struct cli_bc_ctx *cli_bytecode_context_alloc(void);
 /* FIXME: we can't include others.h because others.h includes us...*/
 void cli_bytecode_context_setctx(struct cli_bc_ctx *ctx, void *cctx);
-int cli_bytecode_context_setfuncid(struct cli_bc_ctx *ctx, const struct cli_bc *bc, unsigned funcid);
-int cli_bytecode_context_setparam_int(struct cli_bc_ctx *ctx, unsigned i, uint64_t c);
-int cli_bytecode_context_setparam_ptr(struct cli_bc_ctx *ctx, unsigned i, void *data, unsigned datalen);
-int cli_bytecode_context_setfile(struct cli_bc_ctx *ctx, fmap_t *map);
-int cli_bytecode_context_setpe(struct cli_bc_ctx *ctx, const struct cli_pe_hook_data *data, const struct cli_exe_section *sections);
-int cli_bytecode_context_setpdf(struct cli_bc_ctx *ctx, unsigned phase, unsigned nobjs, struct pdf_obj **objs, uint32_t *pdf_flags, uint32_t pdfsize, uint32_t pdfstartoff);
-int cli_bytecode_context_clear(struct cli_bc_ctx *ctx);
+cl_error_t cli_bytecode_context_setfuncid(struct cli_bc_ctx *ctx, const struct cli_bc *bc, unsigned funcid);
+cl_error_t cli_bytecode_context_setparam_int(struct cli_bc_ctx *ctx, unsigned i, uint64_t c);
+cl_error_t cli_bytecode_context_setparam_ptr(struct cli_bc_ctx *ctx, unsigned i, void *data, unsigned datalen);
+cl_error_t cli_bytecode_context_setfile(struct cli_bc_ctx *ctx, fmap_t *map);
+cl_error_t cli_bytecode_context_setpe(struct cli_bc_ctx *ctx, const struct cli_pe_hook_data *data, const struct cli_exe_section *sections);
+cl_error_t cli_bytecode_context_setpdf(struct cli_bc_ctx *ctx, unsigned phase, unsigned nobjs, struct pdf_obj **objs, uint32_t *pdf_flags, uint32_t pdfsize, uint32_t pdfstartoff);
+cl_error_t cli_bytecode_context_clear(struct cli_bc_ctx *ctx);
+
 /* returns file descriptor, sets tempfile. Caller takes ownership, and is
  * responsible for freeing/unlinking */
 int cli_bytecode_context_getresult_file(struct cli_bc_ctx *ctx, char **tempfilename);
@@ -106,16 +107,16 @@ void cli_bytecode_context_destroy(struct cli_bc_ctx *ctx);
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern LIBCLAMAV_EXPORT int have_clamjit();
+extern LIBCLAMAV_EXPORT bool have_clamjit();
 #ifdef __cplusplus
 }
 #endif
-int cli_bytecode_init(struct cli_all_bc *allbc);
-int cli_bytecode_load(struct cli_bc *bc, FILE *f, struct cli_dbio *dbio, int security, int sigperf);
-int cli_bytecode_prepare2(struct cl_engine *engine, struct cli_all_bc *allbc, unsigned dconfmask);
-int cli_bytecode_run(const struct cli_all_bc *bcs, const struct cli_bc *bc, struct cli_bc_ctx *ctx);
+cl_error_t cli_bytecode_init(struct cli_all_bc *allbc);
+cl_error_t cli_bytecode_load(struct cli_bc *bc, FILE *f, struct cli_dbio *dbio, int security, int sigperf);
+cl_error_t cli_bytecode_prepare2(struct cl_engine *engine, struct cli_all_bc *allbc, unsigned dconfmask);
+cl_error_t cli_bytecode_run(const struct cli_all_bc *bcs, const struct cli_bc *bc, struct cli_bc_ctx *ctx);
 void cli_bytecode_destroy(struct cli_bc *bc);
-int cli_bytecode_done(struct cli_all_bc *allbc);
+cl_error_t cli_bytecode_done(struct cli_all_bc *allbc);
 
 /* Bytecode IR descriptions */
 void cli_bytecode_describe(const struct cli_bc *bc);
@@ -128,14 +129,14 @@ void cli_bytefunc_describe(const struct cli_bc *bc, unsigned funcid);
 struct cli_exe_info;
 struct cli_ctx_tag;
 struct cli_target_info;
-int cli_bytecode_runlsig(struct cli_ctx_tag *ctx, struct cli_target_info *info, const struct cli_all_bc *bcs, unsigned bc_idx, const uint32_t *lsigcnt, const uint32_t *lsigsuboff, fmap_t *map);
-int cli_bytecode_runhook(struct cli_ctx_tag *cctx, const struct cl_engine *engine, struct cli_bc_ctx *ctx, unsigned id, fmap_t *map);
+cl_error_t cli_bytecode_runlsig(struct cli_ctx_tag *ctx, struct cli_target_info *info, const struct cli_all_bc *bcs, unsigned bc_idx, const uint32_t *lsigcnt, const uint32_t *lsigsuboff, fmap_t *map);
+cl_error_t cli_bytecode_runhook(struct cli_ctx_tag *cctx, const struct cl_engine *engine, struct cli_bc_ctx *ctx, unsigned id, fmap_t *map);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int bytecode_init(void);
+cl_error_t bytecode_init(void);
 /* Bytecode internal debug API */
 void cli_bytecode_debug(int argc, char **argv);
 void cli_bytecode_printversion(void);
