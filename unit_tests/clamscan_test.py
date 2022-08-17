21fd0acca4b1a5be10081460c1acb3cb0ffdeb9c
Micah Snyder <micasnyd@cisco.com>
2022-08-16 18:25:55 -0700

    Tests: heuristic alerts with/out allmatch, precedence-mode
    Add new tests to verify correct behaviour combining:
    - a "potentially unwanted" indicator
    - a regular alert (strong indicator)
    
    The test file is a zip that:
    1. first has a malformed PNG file used to trigger the heuristic alert
       when using `--alert-broken-media`.
    2. second has the clam.exe program used to trigger the normal alert.
    
    Included are tests with this file for a number of situations:
    
    1. Test the heuristic alert must not alert because neither allmatch
       is specified, nor --heuristic-scan-precedence.
    
    2. Test the heuristic alert must alert because we don't use the sig
       for the clam.exe file.
    
    3. Test the heuristic alert must alert first because
       --heuristic-scan-precedence is enabled.
       We won't see the other alert because it's not allmatch mode.
    
    4. Test we use --allmatch but we don't use --heuristic-scan-precedence.
       That means the NDB sig should alert first, even though the heuristic
       is encountered first.
       Note the verify_output() uses STRICT_ORDER.
    
    5. Test we use --allmatch AND we use --heuristic-scan-precedence.
       That means the heuristic is encountered first and should be treated
       equally, so it should alert first.
       Note the verify_output() uses STRICT_ORDER.

--- unit_tests/clamscan_test.py
+++ unit_tests/clamscan_test.py
@@ -15,6 +15,7 @@ import unittest
 from zipfile import ZIP_DEFLATED, ZipFile
 
 import testcase
+from testcase import STRICT_ORDER, ANY_ORDER
 
 
 os_platform = platform.platform()
@@ -89,6 +90,20 @@ class TC(testcase.TestCase):
             "*:23db1dd3f77fae25610b6a32701313ae:Test.PESection.Hash.NoSize:73\n"
         )
 
+        # Create a ZIP that has two things:
+        # 1. malformed file tha twill alert with  --alert-broken-media
+        # 2. the clam.exe file that will alert normally.
+        # The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+        # The heuristic alert must behave as intended, depending on whether we use --allmatch, --heuristic-scan-precedence, etc.
+        TC.heuristics_testfile = TC.path_tmp / 'heuristics-test.zip'
+        with ZipFile(str(TC.heuristics_testfile), 'w', ZIP_DEFLATED) as zf:
+            # Add truncted PNG file that will alert with  --alert-broken-media
+            with open(TC.path_source / 'logo.png', 'br') as logo_png:
+                zf.writestr('logo.png.truncated', logo_png.read(6378))
+
+            # Add clam.exe which will alert normally
+            clam_exe = TC.path_build / 'unit_tests' / 'input' / 'clamav_hdb_scanfiles' / 'clam.exe'
+            zf.writestr('clam.exe', clam_exe.read_bytes())
 
     @classmethod
     def tearDownClass(cls):
@@ -529,6 +544,153 @@ class TC(testcase.TestCase):
             'NDB.Clamav-Unit-Test-Signature.UNOFFICIAL FOUND',
         ]
         self.verify_output(output.out, expected=expected_results)
+
+    def test_clamscan_11_heuristic(self):
+        '''
+        This test uses a ZIP that has two things:
+        1. malformed file tha twill alert with  --alert-broken-media
+        2. the clam.exe file that will alert normally.
+        The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+
+        In this test the heuristic alert must not alert because neither allmatch is specified, nor --heuristic-scan-precedence
+        '''
+        self.step_name('Test that a clam heuristic not alert because regular sig alerts first.')
+
+        testfile = TC.heuristics_testfile
+
+        command = '{valgrind} {valgrind_args} {clamscan} -d {clam_exe_db} {testfiles} --alert-broken-media'.format(
+            valgrind=TC.valgrind, valgrind_args=TC.valgrind_args, clamscan=TC.clamscan,
+            clam_exe_db=TC.path_tmp / 'clam.ndb',
+            testfiles=testfile,
+        )
+        output = self.execute_command(command)
+
+        assert output.ec == 1  # virus
+
+        expected_results = ['Test.Pattern.Match.UNOFFICIAL FOUND']
+        unexpected_results = ['Heuristics.Broken.Media.PNG.EOFReadingChunk FOUND']
+        self.verify_output(output.out, expected=expected_results, unexpected=unexpected_results)
+
+    def test_clamscan_11_heuristic_no_other_alert(self):
+        '''
+        This test uses a ZIP that has two things:
+        1. malformed file tha twill alert with  --alert-broken-media
+        2. the clam.exe file that will alert normally.
+        The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+
+        In this test the heuristic alert must alert because we don't use the sig for the other file.
+        '''
+        self.step_name('Test that a clam heuristic will alert, because it is the only detection.')
+
+        testfile = TC.heuristics_testfile
+
+        (TC.path_tmp / 'empty.ndb').write_text(
+            "# Just a comment\n"
+        )
+
+        command = '{valgrind} {valgrind_args} {clamscan} -d {clam_exe_db} {testfiles} --alert-broken-media'.format(
+            valgrind=TC.valgrind, valgrind_args=TC.valgrind_args, clamscan=TC.clamscan,
+            clam_exe_db=TC.path_tmp / 'empty.ndb',
+            testfiles=testfile,
+        )
+        output = self.execute_command(command)
+
+        assert output.ec == 1  # virus
+
+        expected_results = ['Heuristics.Broken.Media.PNG.EOFReadingChunk FOUND']
+        unexpected_results = ['Test.Pattern.Match.UNOFFICIAL FOUND']
+        self.verify_output(output.out, expected=expected_results, unexpected=unexpected_results)
+
+    def test_clamscan_11_heuristic_precedence(self):
+        '''
+        This test uses a ZIP that has two things:
+        1. malformed file tha twill alert with  --alert-broken-media
+        2. the clam.exe file that will alert normally.
+        The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+
+        In this test the heuristic alert must alert first because --heuristic-scan-precedence is enabled.
+        We won't see the other alert because it's not allmatch mode.
+        '''
+        self.step_name('Test that a heuristic-precendence will cause the heuristic alert to happen first, with no other alerts because not allmatch.')
+
+        testfile = TC.heuristics_testfile
+
+        command = '{valgrind} {valgrind_args} {clamscan} -d {clam_exe_db} {testfiles} --alert-broken-media \
+             --heuristic-scan-precedence'.format(
+            valgrind=TC.valgrind, valgrind_args=TC.valgrind_args, clamscan=TC.clamscan,
+            clam_exe_db=TC.path_tmp / 'clam.ndb',
+            testfiles=testfile,
+        )
+        output = self.execute_command(command)
+
+        assert output.ec == 1  # virus
+
+        expected_results = ['Heuristics.Broken.Media.PNG.EOFReadingChunk FOUND']
+        unexpected_results = ['Test.Pattern.Match.UNOFFICIAL FOUND']
+        self.verify_output(output.out, expected=expected_results, unexpected=unexpected_results)
+
+    def test_clamscan_11_heuristic_allmatch(self):
+        '''
+        This test uses a ZIP that has two things:
+        1. malformed file tha twill alert with  --alert-broken-media
+        2. the clam.exe file that will alert normally.
+        The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+
+        In this test we use --allmatch but we don't use --heuristic-scan-precedence.
+        That means the NDB sig should alert first, even though the heuristic is encountered first.
+        Note the verify_output() uses STRICT_ORDER.
+        '''
+        self.step_name('Test that a clam heuristic alert will alert LAST in allmatch mode without heuristic-precedence.')
+
+        testfile = TC.heuristics_testfile
+
+        command = '{valgrind} {valgrind_args} {clamscan} -d {clam_exe_db} {testfiles} --alert-broken-media \
+             --allmatch'.format(
+            valgrind=TC.valgrind, valgrind_args=TC.valgrind_args, clamscan=TC.clamscan,
+            clam_exe_db=TC.path_tmp / 'clam.ndb',
+            testfiles=testfile,
+        )
+        output = self.execute_command(command)
+
+        assert output.ec == 1  # virus
+
+        expected_results = [
+            'Test.Pattern.Match.UNOFFICIAL FOUND',
+            'Heuristics.Broken.Media.PNG.EOFReadingChunk FOUND',
+        ]
+        self.verify_output(output.out, expected=expected_results, order=STRICT_ORDER)
+
+    def test_clamscan_11_heuristic_allmatch_precedence(self):
+        '''
+        This test uses a ZIP that has two things:
+        1. malformed file tha twill alert with  --alert-broken-media
+        2. the clam.exe file that will alert normally.
+        The idea is that since the malformed file is first, the heuristic alert will be encountered first.
+
+        In this test we use --allmatch AND we use --heuristic-scan-precedence.
+        That means the heuristic is encountered first and should be treated equally, so it should alert first.
+        Note the verify_output() uses STRICT_ORDER.
+        '''
+        self.step_name('Test that a clam heuristic alert will alert FIRST in allmatch mode with heuristic-precedence.')
+
+        testfile = TC.heuristics_testfile
+
+        command = '{valgrind} {valgrind_args} {clamscan} -d {clam_exe_db} {testfiles} --alert-broken-media \
+             --allmatch \
+             --heuristic-scan-precedence'.format(
+            valgrind=TC.valgrind, valgrind_args=TC.valgrind_args, clamscan=TC.clamscan,
+            clam_exe_db=TC.path_tmp / 'clam.ndb',
+            testfiles=testfile,
+        )
+        output = self.execute_command(command)
+
+        assert output.ec == 1  # virus
+
+        expected_results = [
+            'Heuristics.Broken.Media.PNG.EOFReadingChunk FOUND',
+            'Test.Pattern.Match.UNOFFICIAL FOUND',
+        ]
+        self.verify_output(output.out, expected=expected_results, order=STRICT_ORDER)
 
     def test_clamscan_12_image_fuzzy_hash_sigs(self):
         self.step_name('Test that each type of hash sig is detected in all-match mode')
