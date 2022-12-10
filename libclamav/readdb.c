e880f22a6ccb70726b9cc741ab3e37dc21e9a237
Shawn Iverson <shawniverson@efa-project.org>
2022-12-09 19:26:38 -0500

    Reverse order of cat and pua_cats

--- libclamav/readdb.c
+++ libclamav/readdb.c
@@ -1221,7 +1221,7 @@ static int cli_chkpua(const char *signame, const char *pua_cats, unsigned int op
     endsig = strrchr(sig, '.');
     strncpy(cat, sig, strlen(sig) - strlen(endsig) + 1);
     cat[strlen(sig) - strlen(endsig) + 1] = 0;
-    cat_pt                = strstr(pua_cats, cat);
+    cat_pt                = strstr(cat, pua_cats);
     cli_dbgmsg("cli_chkpua:                cat=[%s]\n", cat                      );
     cli_dbgmsg("cli_chkpua:                sig=[%s]\n", sig                      );
     if (options & CL_DB_PUA_INCLUDE)
