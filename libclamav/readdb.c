12f4d18ce044adac66797b44818cb42f8f0cf085
Sebastian Andrzej Siewior <sebastian@breakpoint.cc>
2023-02-18 10:47:53 +0100

    libclamav: Use OpenSSL' BN instead tomfastmath.
    Use OpenSSL's big number/ multiprecision integer arithmetics
    functionality to replace tomfastmath.
    
    This is a first shot at doing just this. Further improvement could be
    use more RSA-signature verification from OpenSSL in crtmgr_rsa_verify()
    and less self parsing.
    _padding_check_PKCS1_type_1() has been borrowed from OpenSSL to make
    further replacments easier.
    
    Signed-off-by: Sebastian Andrzej Siewior <sebastian@breakpoint.cc>

--- libclamav/readdb.c
+++ libclamav/readdb.c
@@ -3319,9 +3319,7 @@ static int cli_loadcrt(FILE *fs, struct cl_engine *engine, struct cli_dbio *dbio
     char *tokens[CRT_TOKENS + 1];
     size_t line = 0, tokens_count;
     cli_crt ca;
-    int ret             = CL_SUCCESS;
-    char *pubkey        = NULL;
-    const uint8_t exp[] = "\x01\x00\x01";
+    int ret = CL_SUCCESS;
 
     if (!(engine->dconf->pe & PE_CONF_CERTS)) {
         cli_dbgmsg("cli_loadcrt: Ignoring .crb sigs due to DCONF configuration\n");
@@ -3333,7 +3331,10 @@ static int cli_loadcrt(FILE *fs, struct cl_engine *engine, struct cli_dbio *dbio
         return ret;
     }
 
-    cli_crt_init(&ca);
+    if (cli_crt_init(&ca) < 0) {
+        cli_dbgmsg("cli_loadcrt: No mem for CA init.\n");
+        return CL_EMEM;
+    }
     memset(ca.issuer, 0xca, sizeof(ca.issuer));
 
     while (cli_dbgets(buffer, FILEBUFF, fs, dbio)) {
@@ -3411,25 +3412,16 @@ static int cli_loadcrt(FILE *fs, struct cl_engine *engine, struct cli_dbio *dbio
             goto done;
         }
 
-        pubkey = cli_hex2str(tokens[4]);
-        if (!pubkey) {
+        if (BN_hex2bn(&ca.n, tokens[4]) == 0) {
             cli_errmsg("cli_loadcrt: line %u: Cannot convert public key to binary string\n", (unsigned int)line);
             ret = CL_EMALFDB;
             goto done;
         }
-
-        /*
-         * tokens[4] is the public key.  having a length that is too
-         * long causes an out of bounds read in the this call.
-         */
-        if ((strlen(tokens[4]) / 2) >= (FP_MAX_SIZE / 8)) {
-            cli_errmsg("cli_loadcrt: line %u: Public key too long.\nNOTE: If this is actually a valid key length, recompile with a larger FP_MAX_SIZE (currently %d).\n", (unsigned int)line, FP_MAX_SIZE);
-            ret = CL_EMALFDB;
+        /* Set the RSA exponent of 65537 */
+        if (!BN_set_word(ca.e, 65537)) {
+            cli_errmsg("cli_loadcrt: Cannot set the exponent.\n");
             goto done;
         }
-        fp_read_unsigned_bin(&(ca.n), (const unsigned char *)pubkey, strlen(tokens[4]) / 2);
-
-        fp_read_unsigned_bin(&(ca.e), exp, sizeof(exp) - 1);
 
         switch (tokens[6][0]) {
             case '1':
@@ -3481,13 +3473,9 @@ static int cli_loadcrt(FILE *fs, struct cl_engine *engine, struct cli_dbio *dbio
 
         ca.hashtype = CLI_HASHTYPE_ANY;
         crtmgr_add(&(engine->cmgr), &ca);
-
-        FREE(pubkey);
     }
 
 done:
-    FREE(pubkey);
-
     cli_dbgmsg("Number of certs: %d\n", engine->cmgr.items);
     cli_crt_clear(&ca);
     return ret;
