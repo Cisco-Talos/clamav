e0342713f33ca59c254a185d05a9254c44453307
Micah Snyder <micasnyd@cisco.com>
2023-08-07 16:59:43 -0700

    Coverity-415954: Remove duplicated/dead code

--- libclamav/egg.c
+++ libclamav/egg.c
@@ -970,8 +970,6 @@ static void print_posix_info_mode(uint32_t mode)
         printf("p");
     } else if (mode & POSIX_INFO_MODE_SYM_LINK) {
         printf("l");
-    } else if (mode & POSIX_INFO_MODE_SOCKET) {
-        printf("s");
     }
     /* Owner/Group/Other permissions */
     if (mode & POSIX_INFO_MODE_PERM_OWNER_READ) {
