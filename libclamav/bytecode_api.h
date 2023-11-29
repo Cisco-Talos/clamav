d6e938c6052f279b8ef231944a7bddbf76994cd9
Micah Snyder <micasnyd@cisco.com>
2023-11-29 12:27:38 -0500

    Bump version to 1.0.5 and FLEVEL to 165

--- libclamav/bytecode_api.h
+++ libclamav/bytecode_api.h
@@ -168,6 +168,7 @@ enum FunctionalityLevels {
     FUNC_LEVEL_1_0_2 = 162, /**< LibClamAV release 1.0.2 */
     FUNC_LEVEL_1_0_3 = 163, /**< LibClamAV release 1.0.3 */
     FUNC_LEVEL_1_0_4 = 164, /**< LibClamAV release 1.0.4 */
+    FUNC_LEVEL_1_0_5 = 165, /**< LibClamAV release 1.0.5 */
 };
 
 /**
--- libclamav/others.h
+++ libclamav/others.h
@@ -73,7 +73,7 @@
  * in re-enabling affected modules.
  */
 
-#define CL_FLEVEL 164
+#define CL_FLEVEL 165
 #define CL_FLEVEL_DCONF CL_FLEVEL
 #define CL_FLEVEL_SIGTOOL CL_FLEVEL
 
