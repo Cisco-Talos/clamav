6aca4ad7d93560c0af5a5dd94439dc485b3c8abe
ragusaa <54862477+ragusaa@users.noreply.github.com>
2024-03-15 13:47:36 -0400

    Update libclamav_rust/src/alz.rs
    Co-authored-by: Micah Snyder <micah.d.snyder@gmail.com>

--- libclamav_rust/src/alz.rs
+++ libclamav_rust/src/alz.rs
@@ -1,4 +1,26 @@
 /*
+ *  ALZ archive extraction. 
+ *
+ *  Copyright (C) 2024 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+ *
+ *  Authors: Andy Ragusa
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+
+/*
 #![warn(
     clippy::all,
     clippy::restriction,
