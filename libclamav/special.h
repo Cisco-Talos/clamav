9406804a1f0c07385212f2c320dc38a127524bdc
John Humlick <15677335+jhumlick@users.noreply.github.com>
2025-07-17 14:34:28 -0700

    libclamav: Remove MyDoom Heuristic The MyDoom heuristic has been causing too many false positives. Since we already have MyDoom coverage through signature detection, the hard-coded heuristic that causes many false positives is no longer needed. This commit removes the hard-coded heuristic.
    CLAM-2766

--- libclamav/special.h
+++ libclamav/special.h
@@ -34,7 +34,6 @@ struct swizz_stats {
     int entries;
 };
 
-int cli_check_mydoom_log(cli_ctx *ctx);
 int cli_check_riff_exploit(cli_ctx *ctx);
 void cli_detect_swizz_str(const unsigned char *str, uint32_t len, struct swizz_stats *stats, int blob);
 int cli_detect_swizz(struct swizz_stats *stats);
