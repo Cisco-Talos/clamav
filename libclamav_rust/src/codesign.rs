c7d937030b1118fc84ac4069ea738104a66b4f7b
Valerie Snyder <mx.val@icloud.com>
2026-02-03 12:00:21 -0500

    Ignore cert files in CVD certs directory which can't be read
    The default CVD certs directory on unix systems is `/usr/local/etc/certs`, which
    is not unique to ClamAV and may already exist and include other certificates,
    some of which may have restricted user permissions inaccessible to the running
    ClamAV application. In this scenario, ClamAV should skip over the cert files it
    cannot read.
    
    Resolves: https://github.com/Cisco-Talos/clamav/issues/1665
    
    CLAM-2942

--- libclamav_rust/src/codesign.rs
+++ libclamav_rust/src/codesign.rs
@@ -576,7 +576,13 @@ impl Verifier {
             if path.is_file() {
                 let ext = path.extension();
                 if ext.is_some() && (ext.unwrap() == "pem" || ext.unwrap() == "crt") {
-                    let certs_in_file = X509::stack_from_pem(&std::fs::read(path)?)?;
+                    let read_result = std::fs::read(&path);
+                    if let Err(e) = read_result {
+                        debug!("Error reading certificate file '{:?}': {}", path, e);
+                        continue;
+                    }
+                    let certs_in_file = X509::stack_from_pem(&read_result.unwrap())?;
+
                     for cert in certs_in_file {
                         // get cert common name
                         let common_name = cert
